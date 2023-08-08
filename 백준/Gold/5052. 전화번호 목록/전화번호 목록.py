77466cb73fb830d34c87717be3ceb2dd8f28b794
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2023-08-08 10:07:26 +0900

    [Gold IV] Title: 전화번호 목록, Time: 144 ms, Memory: 32276 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
@@ -1,56 +1,24 @@
 import sys
 input = sys.stdin.readline
 
-class Node(object):
-    def __init__(self, key, data=None):
-        self.key = key
-        self.data = data
-        self.children = {}
+def main():
+    t = int(input())
 
-class Trie(object):
-    def __init__(self):
-        self.head = Node(None)
+    for _ in range(t):
+        n = int(input())
+        nums = [input().rstrip() for _ in range(n)]
+        nums.sort()
 
-    def insert(self, string):
-        cur_node = self.head
+        flag = True
+        for i in range(n-1):
+            length = len(nums[i])
+            if nums[i] == nums[i+1][:length]:
+                flag = False
+                break
 
-        for char in string:
-            if char not in cur_node.children:
-                cur_node.children[char] = Node(char)
-            cur_node = cur_node.children[char]
-
-        cur_node.data = string
-
-    def search_prefix(self, string):
-        cur_node = self.head
-
-        for char in string:
-            cur_node = cur_node.children[char]
-
-        if cur_node.children:
-            return False
+        if flag:
+            print('YES')
         else:
-            return True
+            print('NO')
 
-t = int(input())
-
-
-for _ in range(t):
-    n = int(input())
-    trie = Trie()
-    nums = []
-    for _ in range(n):
-        num = input().rstrip()
-        nums.append(num)
-        trie.insert(num)
-
-    isPossible = True
-    for num in nums:
-        if not trie.search_prefix(num):
-            isPossible = False
-            break
-
-    if isPossible:
-        print('YES')
-    else:
-        print('NO')
+main()
