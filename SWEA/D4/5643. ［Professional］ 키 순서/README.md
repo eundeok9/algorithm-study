6d98d1daf43116ff247be9eff3281f51f40b8b65
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2023-10-04 21:46:32 +0900

    [D4] Title: [Professional] 키 순서, Time: 9,056 ms, Memory: 75,184 KB -BaekjoonHub

---
+++ SWEA/D4/5643. ［Professional］ 키 순서/README.md
@@ -0,0 +1,11 @@
+# [D4] [Professional] 키 순서 - 5643 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWXQsLWKd5cDFAUo) 
+
+### 성능 요약
+
+메모리: 75,184 KB, 시간: 9,056 ms, 코드길이: 700 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.py
@@ -0,0 +1,29 @@
+# import sys
+# input = sys.stdin.readline
+
+t = int(input())
+for tc in range(1, t+1):
+    n = int(input())
+    m = int(input())
+    compare = [[0 for _ in range(n)] for _ in range(n)]
+    for _ in range(m):
+        small, tall = map(int, input().split())
+        compare[small-1][tall-1] = 1
+
+
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                if compare[i][k] == 1 and compare[k][j] == 1:
+                    compare[i][j] = 1
+
+    ans = 0
+    for i in range(n):
+        possible = 0
+        for j in range(n):
+            possible += compare[i][j] + compare[j][i]
+
+        if possible == (n-1):
+            ans += 1
+
+    print("#{} {}".format(tc, ans))
