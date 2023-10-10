b1aefea0c39a93a0e240b669a676aad8a004911b
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2023-10-10 16:28:42 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 247 ms, Memory: 63,580 KB -BaekjoonHub

--- SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.py
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.py
@@ -1,33 +1,37 @@
-
-def dfs(i):
-    visited[i] = 1
-
-    for node in graph[i]:
-        if visited[node]:
-            continue
-        dfs(node)
+# 창용 마을 무리의 개수
 
 t = int(input())
 
+def dfs(cur):
+    visited[cur] = 1
+
+    for neighbor in graph[cur]:
+        if visited[neighbor] == 1:
+            continue
+        else:
+            dfs(neighbor)
+
+
 for tc in range(1, t+1):
+
     n, m = list(map(int, input().split()))
 
     graph = [[] for _ in range(n)]
 
     for _ in range(m):
-        a, b = list(map(int, input().split()))
-        a -= 1 # 0부터 시작이라서 1씩 빼줌
+        a, b = list(map(int ,input().split()))
+        a -= 1
         b -= 1
         graph[a].append(b)
         graph[b].append(a)
 
-    visited = [0 for _ in range(n)]
-    cnt = 0 # 무리 개수
 
+    visited = [0 for _ in range(n)]
+
+    cnt = 0
     for i in range(n):
-        if visited[i]:
-            continue
-        dfs(i)
-        cnt += 1
+        if visited[i] == 0:
+            dfs(i)
+            cnt += 1
 
     print("#{} {}".format(tc, cnt))
