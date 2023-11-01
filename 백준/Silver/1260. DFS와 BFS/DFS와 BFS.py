f1b63d7b982a58f592f4a6f1ecb746cb8cbe5282
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2023-11-02 00:49:08 +0900

    [Silver II] Title: DFS와 BFS, Time: 80 ms, Memory: 34052 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,49 +1,45 @@
-# 1260번. DFS와 BFS
-# 문제) 그래프를 DFS로 탐색한 결과와 BFS로 탐색한 결과를 출력하는 프로그램 작성
-# 입력) 정점의 개수: N, 간선의 개수 M, 탐색을 시작할 번호 V
-# 출력) 첫째 줄에 DFS 수행한 결과, 다음 줄에 BFS 수행한 결과
-# 조건) 시간 제한: 2초, 메모리 제한: 128MB
-
 from collections import deque
-def dfs(start):
-    visited1[start] = True
-    print(start, end=' ')
-    for i in graph[start]:
-        if not visited1[i]:
-            dfs(i)
+import sys
+input = sys.stdin.readline
 
+def dfs(v):
+    visited[v] = True
+    print(v, end=" ")
 
-def bfs(start):
-    queue = deque([start])
-    visited2[start] = True
+    graph[v].sort()
+    for nxt in graph[v]:
+        if not visited[nxt]:
+            dfs(nxt)
+
+def bfs(v):
+    queue = deque()
+    queue.append(v)
+    visited[v] = True # 방문 처리 유의하기~
 
     while queue:
-        v = queue.popleft()
-        print(v, end=' ')
-        for i in graph[v]:
-            if not visited2[i]:
-                queue.append(i)
-                visited2[i] = True
+        now = queue.popleft()
+        print(now, end=" ")
+        graph[now].sort()
+        for nxt in graph[now]:
+            if not visited[nxt]:
+                visited[nxt] = True # 방문 처리 유의하기~
+                queue.append(nxt)
 
 
-n, m, v = map(int,input().split())
+# n: 정점의 개수
+# m: 간선의 개수
+# v: 시작 정점
+n, m, v = map(int, input().split())
 
 graph = [[] for _ in range(n+1)]
-
-visited1 = [False] * (n+1) # dfs를 위한 방문처리
-visited2 = [False] * (n+1) # bfs를 위한 방문처리
 
 for _ in range(m):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
 
-for i in graph:
-    i.sort()
-
+visited = [False for _ in range(n+1)]
 dfs(v)
 print()
+visited = [False for _ in range(n+1)]
 bfs(v)
-
-
-
