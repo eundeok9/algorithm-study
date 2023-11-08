f6572abdca33c082b798c23d4eb51d89aa782bf3
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2023-11-08 20:39:51 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벽돌 깨기, Time: 4,745 ms, Memory: 71,736 KB -BaekjoonHub

--- SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.py
+++ SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.py
@@ -1,113 +1,93 @@
-# 벽돌 깨기, 15초(50개 테스트케이스)
-# n개의 구슬로 최대한 많은 벽돌을 깨서 남은 벽돌 개수 구하기
-from collections import deque
+# 벽돌 깨기
 from itertools import product
-from itertools import combinations
+from collections import deque
+# 풀이 방법
+# 1. w칸 중에 구슬을 때릴 칸을 n번 선택한다 (wPn) -> perm 배열에 저장
+# 2. perm 배열에서 숫자 하나씩 꺼내고, 해당 숫자에 해당하는 열 위에서부터 탐색 -> 0이 아닌 수 만나면 queue에 (좌표, 숫자) 넣기
+# 3. 벽돌을 깨자, queue에서 값을 꺼내자
+#   - 벽돌에 적힌 숫자가 1이면: 걔만 0으로 바꾸고 perm에서 다음 숫자 꺼내
+#   - 1이 아니면: 걔를 0으로 바꾸고 사방 탐색을 (숫자-1)만큼 해야됨
 
 
+def break_brick(arr):
+    # 벽돌 배열 복사해오기
+    # 이제 여기선 복사된 배열만 쓸거임.. 아마도?..
+    bricksCopy = [[0 for _ in range(w)] for _ in range(h)]
+    for r in range(h):
+        for c in range(w):
+            bricksCopy[r][c] = bricks[r][c]
 
-# 중복 순열
-def perm():
-    global minBricks
-    col_list = [i for i in range(w)]
-    for numbers in list(product(col_list, repeat=n)):
-        minBricks = min(hit(numbers), minBricks)
-
-
-# 벽돌 때리기
-# - 때릴 위치가 저장되어있는 numbers의 값을 하나씩 빼와서
-# - 해당 열의 가장 위에 있는 벽돌을 때린다
-# - 그리고 그 벽돌에 적힌 숫자에 해당하는 범위만큼 벽돌을 없앤다
-def hit(numbers):
-    bricks_copy = [[0 for _ in range(w)] for _ in range(h)] # 복사본
-    for i in range(h):
-        for j in range(w):
-            bricks_copy[i][j] = bricks[i][j]
     queue = deque()
+    # print(arr, "일 때")
+    # print("--------")
+    for colNum in arr:
+        for row in range(h):
+            if bricksCopy[row][colNum] != 0: # 이 열에서 벽돌을 찾았다!
+                queue.append((row, colNum, bricksCopy[row][colNum]))
 
-    for col in numbers:
-        for r in range(h):
-            if bricks_copy[r][col]:
-                number = bricks_copy[r][col] # 벽돌에 적힌 숫자
-
-                queue.append((r, col, number))
-                bricks_copy[r][col] = 0
-
-                while queue:
+                while queue: # 큐를 돌아보자..
                     size = len(queue)
-                    for i in range(size):
-                        x, y, num = queue.popleft()
+                    for _ in range(size):
+                        r, c, num = queue.popleft()
 
-                        for dir in range(4):  # 4방 탐색
-                            for cnt in range(num):  # 벽돌에 적힌 수 만큼 이동
-                                nx = x + dx[dir] * cnt
-                                ny = y + dy[dir] * cnt
-                                if 0 > nx or nx >= h or ny < 0 or ny >= w:
-                                    continue
-                                if bricks_copy[nx][ny] == 0:
-                                    continue
-                                queue.append((nx, ny, bricks_copy[nx][ny]))
-                                bricks_copy[nx][ny] = 0
+                        bricksCopy[r][c] = 0
+                        for d in range(4):
+                            for i in range(1,num):
+                                nr = r + dx[d] * i
+                                nc = c + dy[d] * i
+                                if 0 <= nr < h and 0 <= nc < w and bricksCopy[nr][nc] != 0:
+                                    queue.append((nr, nc, bricksCopy[nr][nc]))
+                                    bricksCopy[nr][nc] = 0
+                break # 젤 위에 하나 깼으면 끝내고 벽돌 내리고 perm 배열에서 다음 값 꺼내
 
-
-                break
-
-        falling_bricks(bricks_copy)
-
-    return count_bricks(bricks_copy)
+        # 잘 깨지는지 중간 점검
+        # for row in bricksCopy:
+        #     print(row)
+        # print("- - - - - - - -")
+        fall_brick(bricksCopy)
 
 
-# 빈 칸으로 벽돌 떨어트리기
-def falling_bricks(bricks_copy):
-    for i in range(w):
-        for j in range(h-1, -1, -1):
-            if bricks_copy[j][i] == 0:
-                for k in range(j-1, -1, -1):
-                    if bricks_copy[k][i] != 0:
-                        bricks_copy[j][i] = bricks_copy[k][i]
-                        bricks_copy[k][i] = 0
-                        break
 
+    # 다 끝나면 남은 벽돌 개수 세자
+    cnt = count_remain_brick(bricksCopy)
 
-def count_bricks(bricks_copy):
-    remain_bricks = 0
-    for i in range(h):
-        for j in range(w):
-            if bricks_copy[i][j] != 0:
-                remain_bricks += 1
+    return cnt
 
-    return remain_bricks
+def fall_brick(bricks):
+    for col in range(w):
+        for i in range(h-1, 0, -1):
+            if bricks[i][col] == 0:
+                for j in range(i-1, -1,-1):
+                    if bricks[j][col] == 0:
+                        continue
+                    elif bricks[j][col] != 0:
+                        bricks[i][col] = bricks[j][col]
+                        bricks[j][col] = 0
+                        i = j+1
+                    break
 
+def count_remain_brick(bricks):
+    cnt = 0
+    for r in range(h):
+        for c in range(w):
+           if bricks[r][c] != 0:
+               cnt += 1
+    return cnt
 
-# 상, 하, 좌, 우
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+# 좌 하 우 상
+dx = [0, 1, 0, -1]
+dy = [-1, 0, 1, 0]
 
-t = int(input())
-for tc in range(1, t+1):
-    # n : 구슬 개수
-    # w : 가로 너비 (열)
-    # h : 세로 높이 (행)
-    n, w, h = list(map(int, input().split()))
+test_case = int(input())
 
-    # 벽돌 입력 받기
-    bricks = [] # 벽돌
+for tc in range(1, test_case+1):
+    n, w, h = map(int, input().split())
+    bricks = [list(map(int, input().split())) for _ in range(h)]
 
-    for _ in range(h):
-        bricks.append(list(map(int, input().split())))
+    minCnt = 1e8
+    num_arr = [int(i) for i in range(w)]
+    for perm in product(num_arr, repeat=n):
+        minCnt = min(break_brick(perm), minCnt)
 
-    # 전체 벽돌의 개수
-    # total_bricks = 0
-    # for i in range(h):
-    #     for j in range(w):
-    #         if bricks[i][j]:
-    #             total_bricks += 1
-
-    numbers = [0 for _ in range(n)]
-
-    # 중복 순열로 타격할 열 위치 구하고, 깨트린 벽돌 개수 구하기
-    minBricks = 9999
-    perm()
-
-    print("#{} {}".format(tc, minBricks))
+    print("#{} {}".format(tc, minCnt))
 
