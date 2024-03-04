abda655729ee9ce08185abb21726f8d78f51b914
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2024-03-04 22:01:06 +0900

    [Silver III] Title: N과 M (3), Time: 1848 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -1,8 +1,10 @@
 from itertools import product
 
 n, m = list(map(int, input().split()))
-num_list = [i for i in range(1,n+1)]
-numbers = product(num_list, repeat=m)
+
+arr = [i for i in range(1, n+1)]
+
+numbers = product(arr, repeat=m)
 
 for number in numbers:
     print(*number)
