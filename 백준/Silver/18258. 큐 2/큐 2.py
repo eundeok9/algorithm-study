2e460ec37c3fd87c831f2fc6f4335342f8dfff73
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2024-08-09 20:42:00 +0900

    [Silver IV] Title: 큐 2, Time: 1528 ms, Memory: 175116 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -0,0 +1,40 @@
+# 0.5초, 256mb
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+n =int(input())
+queue = deque()
+
+for _ in range(n):
+    cmd = list(input().rstrip().split())
+    
+    if cmd[0] == 'push':
+        queue.append(cmd[1])
+    
+    elif cmd[0] == 'pop':
+        if queue:
+            print(queue.popleft())
+        else:
+            print(-1)
+    
+    elif cmd[0] == 'size':
+        print(len(queue))
+    
+    elif cmd[0] == 'empty':
+        if queue:
+            print(0)
+        else:
+            print(1)
+        
+    elif cmd[0] == 'front':
+        if queue:
+            print(queue[0])
+        else:
+            print(-1)
+    
+    elif cmd[0] == 'back':
+        if queue:
+            print(queue[-1])
+        else:
+            print(-1)
