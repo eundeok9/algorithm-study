dc8b5db5d9394f33c9a7b2b1a5ad6ab6069ecca5
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2024-08-22 13:24:23 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -0,0 +1,18 @@
+# 1초, 512mb
+import sys
+input = sys.stdin.readline
+
+tc = int(input())
+
+for _ in range(tc):
+    n = int(input())
+    
+    dp = [0 for _ in range(12)]
+    dp[1] = 1
+    dp[2] = 2
+    dp[3] = 4
+
+    for i in range(4, n+1):
+        dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+        
+    print(dp[n])
