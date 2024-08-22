9e0f29b79d0704a61949954307d2e61628101e18
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2024-08-22 13:40:14 +0900

    [Silver II] Title: 최소 힙, Time: 156 ms, Memory: 37044 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,18 @@
+# 1초, 128mb
+import sys
+input = sys.stdin.readline
+import heapq
+
+n = int(input())
+
+heap = []
+for _ in range(n):
+    x = int(input())
+    
+    if x != 0:
+        heapq.heappush(heap, x)
+    else:
+        try:
+            print(heapq.heappop(heap))
+        except:
+            print(0)
