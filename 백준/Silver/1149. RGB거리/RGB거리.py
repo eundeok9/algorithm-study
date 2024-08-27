2fcc9740eeb276f2b0a701ecde4c491117e011d3
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2024-08-27 15:50:12 +0900

    [Silver I] Title: RGB거리, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,17 +1,18 @@
+# 0.5초, 128mb
+
 import sys
 input = sys.stdin.readline
 
-table = []
-
 n = int(input())
 
-for _ in range(n):
-    table.append(list(map(int, input().split())))
+dp = []
+
+for i in range(n):
+    dp.append(list(map(int, input().split())))
 
 for i in range(1, n):
-    table[i][0] = min(table[i - 1][1], table[i - 1][2]) + table[i][0]
-    table[i][1] = min(table[i - 1][0], table[i - 1][2]) + table[i][1]
-    table[i][2] = min(table[i - 1][1], table[i - 1][0]) + table[i][2]
+    dp[i][0] = dp[i][0] + min(dp[i-1][1], dp[i-1][2])
+    dp[i][1] = dp[i][1] + min(dp[i-1][0], dp[i-1][2])
+    dp[i][2] = dp[i][2] + min(dp[i-1][0], dp[i-1][1])
 
-cost = min(table[n-1][0], table[n-1][1], table[n-1][2])
-print(cost)
+print(min(dp[n-1]))
