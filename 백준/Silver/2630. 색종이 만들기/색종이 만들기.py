5d28df0680fce95697534583a4fbfc4bf67befee
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2024-08-29 17:31:33 +0900

    [Silver II] Title: 색종이 만들기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -7,23 +7,23 @@ paper = [list(map(int, input().split())) for _ in range(n)]
 
 result = []
 
-def solution(x, y, n):
+
+def sol(x, y, n):
     color = paper[x][y]
     for i in range(x, x+n):
         for j in range(y, y+n):
             if color != paper[i][j]:
-                solution(x, y, n//2)
-                solution(x, y+n//2, n//2)
-                solution(x+n//2, y, n//2)
-                solution(x+n//2, y+n//2, n//2)
+                sol(x, y, n//2)
+                sol(x, y+n//2, n//2)
+                sol(x+n//2, y, n//2)
+                sol(x+n//2, y+n//2, n//2)
                 return
-            
+    
     if color == 0:
         result.append(0)
-
     else:
         result.append(1)
 
-solution(0, 0, n)
+sol(0, 0, n)
 print(result.count(0))
 print(result.count(1))
