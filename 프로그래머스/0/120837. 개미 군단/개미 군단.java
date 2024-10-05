c63eac5b22f4164fcdede3d02124c229b4b28adc
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2024-10-05 22:47:56 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -5,11 +5,11 @@ class Solution {
         while(hp > 0) {
             if(hp >= 5) {
                 answer += hp / 5;
-                hp -= 5 * (hp / 5);
+                hp %= 5;
             }
             else if(hp >= 3) {
                 answer += hp / 3;
-                hp -= 3 * (hp / 3);
+                hp %= 3;
             } else {
                 answer += hp;
                 break;
