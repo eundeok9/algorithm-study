9ca2894cac2d9dad127c3653faef874095c60e4a
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2024-10-06 01:31:52 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -1,13 +1,13 @@
 class Solution {
     public int solution(int a, int b, int c) {
-        int answer = 0;
-        
-        if(a!=b && b!=c && a!=c) {
-            return a+b+c;
-        } else if((a==b && b!=c && a!=c) || (a!=b && b==c && a!=c) || (a!=b && b!=c && a==c)) {
-            return (a+b+c) * (a*a + b*b + c*c);
-        } else if (a==b && b==c && a==c) {
-            return (a+b+c) * (a*a + b*b + c*c) * (a*a*a + b*b*b + c*c*c);
+        int answer = a + b + c;
+
+        if (a == b || b == c || a == c) {
+            answer *= (a * a + b * b + c * c);
+        }
+
+        if (a == b && b == c) {
+            answer *= (a * a * a + b * b * b + c * c * c);
         }
         
         return answer;
