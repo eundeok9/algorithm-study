8d62401cf41cbed49a8acef5c4c7d9a6a706e94f
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2024-10-07 21:39:57 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 36.32 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,21 +1,14 @@
 import java.util.*;
 
 public class Solution {
-    public int[] solution(int []arr) {
+    public Stack<Integer> solution(int []arr) {
         Stack<Integer> stack = new Stack<>();
         
         for(int num: arr) {
             if(!stack.empty() && stack.peek() == num) continue;
             stack.push(num);
         }
-        
-        int[] answer = new int[stack.size()];
-        int size = stack.size();
-        
-        for(int i=size-1; i>=0; i--) {
-            answer[i] = stack.pop();
-        }
 
-        return answer;
+        return stack;
     }
 }
