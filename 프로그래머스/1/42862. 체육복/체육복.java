7459af3cf7ab58a16d06e82f744d1d379ba337f3
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2024-10-07 23:38:39 +0900

    [level 1] Title: 체육복, Time: 0.53 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,31 +2,29 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int answer = 0;
-
+        int answer = n - lost.length;
+        
         Arrays.sort(lost);
         Arrays.sort(reserve);
         
-        answer = n - lost.length; // 도난당하지 않은 학생 수
-        
-        // 도난 당한 학생 중 여벌 체육복이 있는 학생
+        // 도난 당했는데 여분 체육복이 있을 때
         for(int i=0; i<lost.length; i++) {
             for(int j=0; j<reserve.length; j++) {
                 if(lost[i] == reserve[j]) {
-                    answer++;
-                    lost[i] = -1; // 빌림 체크
-                    reserve[j] = -1; // 빌려줌 체크
+                    lost[i] = -1;
+                    reserve[j] = -1;
+                    answer += 1;
                     break;
                 }
             }
         }
         
-        // 도난 당한 학생 중 체육복을 빌릴 수 있는 학생
+        // 도난 당했는데 여분 체육복 없을 때
         for(int i=0; i<lost.length; i++) {
             for(int j=0; j<reserve.length; j++) {
-                if(lost[i] -1 == reserve[j] || lost[i] + 1 == reserve[j]) {
-                    answer++;
-                    reserve[j] = -1; // 빌려줌 체크
+                if(lost[i] - 1 == reserve[j] || lost[i] + 1 == reserve[j]) {
+                    reserve[j] = -1;
+                    answer += 1;
                     break;
                 }
             }
