5bae854323174f6f00ccb037c88099d6a2aa1137
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2024-10-09 23:37:05 +0900

    [level 2] Title: 소수 찾기, Time: 32.42 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,15 +1,15 @@
 import java.util.*;
 
 class Solution {
+    static boolean[] isVisited = new boolean[7];
     static HashSet<Integer> set;
-    static boolean[] visited = new boolean[7];
     
     public int solution(String numbers) {
         int answer = 0;
         set = new HashSet<>();
-        dfs(numbers, "", 0);
+        backTracking(numbers, "", 0);
         
-        for(Integer num: set) {
+        for(int num: set) {
             if(isPrime(num)) {
                 answer++;
             }
@@ -18,17 +18,17 @@ class Solution {
         return answer;
     }
     
-    public static void dfs(String numbers, String s, int depth) {
+    public static void backTracking(String numbers, String s, int depth) {
         if(depth == numbers.length()) {
             return;
         }
         
         for(int i=0; i<numbers.length(); i++) {
-            if(!visited[i]) {
-                visited[i] = true;
+            if(!isVisited[i]) {
+                isVisited[i] = true;
                 set.add(Integer.parseInt(s + numbers.charAt(i)));
-                dfs(numbers, s+numbers.charAt(i), depth+1);
-                visited[i] = false;
+                backTracking(numbers, s+numbers.charAt(i), depth+1);
+                isVisited[i] = false;
             }
         }
     }
@@ -38,7 +38,7 @@ class Solution {
             return false;
         }
         
-        for(int i=2; i<=Math.sqrt(num); i++) {
+        for(int i=2; i*i<=num; i++) {
             if(num%i==0) {
                 return false;
             }
