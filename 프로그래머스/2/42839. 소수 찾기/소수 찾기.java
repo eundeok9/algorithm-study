669a37db17d332ad2e3360bfad1f451a15a1f33d
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2024-10-09 15:55:23 +0900

    [level 2] Title: 소수 찾기, Time: 23.58 ms, Memory: 88.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class Solution {
+    static HashSet<Integer> set;
+    static boolean[] visited = new boolean[7];
+    
+    public int solution(String numbers) {
+        int answer = 0;
+        set = new HashSet<>();
+        dfs(numbers, "", 0);
+        
+        for(Integer num: set) {
+            if(isPrime(num)) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+    
+    public static void dfs(String numbers, String s, int depth) {
+        if(depth == numbers.length()) {
+            return;
+        }
+        
+        for(int i=0; i<numbers.length(); i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                set.add(Integer.parseInt(s + numbers.charAt(i)));
+                dfs(numbers, s+numbers.charAt(i), depth+1);
+                visited[i] = false;
+            }
+        }
+    }
+    
+    public static boolean isPrime(int num) {
+        if (num < 2) {
+            return false;
+        }
+        
+        for(int i=2; i<=Math.sqrt(num); i++) {
+            if(num%i==0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
