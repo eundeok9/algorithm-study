0f56b11fc3a5d17978919fbef1a50d82255f7f8a
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2024-10-12 23:38:19 +0900

    [level 3] Title: 단어 변환, Time: 0.33 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -5,29 +5,29 @@ class Solution {
     public int solution(String begin, String target, String[] words) {
         visited = new boolean[words.length];
         
-        dfs(begin, target, words, 0);
+        dfs(begin, target, words,0);
         
         return answer;
     }
     
-    public static void dfs(String begin, String target, String[] words, int cnt) {
-        if (begin.equals(target)) {
-            answer = cnt;
+    public static void dfs(String begin, String target, String[] words, int count) {
+        if(begin.equals(target)) {
+            answer = count;
             return;
         }
         
-        for(int i=0; i< words.length; i++) {
+        for(int i=0; i<words.length; i++) {
             if(!visited[i]) {
-                int k = 0; // 같은 스펠링 개수
-                for(int j=0; j<begin.length(); j++) {
-                    if(begin.charAt(j) == words[i].charAt(j)) {
+                int k=0;
+                for(int j=0; j<words[i].length(); j++) {
+                    if(words[i].charAt(j) == begin.charAt(j)) {
                         k++;
                     }
                 }
                 
-                if(k==begin.length() -1) { // 한글자 빼고 같은 경우
+                if(k==words[i].length()-1) {
                     visited[i] = true;
-                    dfs(words[i], target, words, cnt+1);
+                    dfs(words[i], target, words, count+1);
                     visited[i] = false;
                 }
             }
