df3d87024ed80754eaf625bdf703055218388f9e
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2024-10-12 14:43:11 +0900

    [level 2] Title: 피로도, Time: 3.21 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,20 +1,18 @@
 class Solution {
-    static int answer = 0;
+    static int answer = -1;
     static boolean[] visited;
+    
     public int solution(int k, int[][] dungeons) {
         visited = new boolean[dungeons.length];
-        
-        dfs(0, k, dungeons);
-        
+        dfs(dungeons, k, 0);
         return answer;
     }
     
-    
-    static public void dfs(int depth, int k, int[][] dungeons) {
+    public static void dfs(int[][] dungeons, int k, int depth) {
         for(int i=0; i<dungeons.length; i++) {
             if(!visited[i] && dungeons[i][0] <= k) {
                 visited[i] = true;
-                dfs(depth+1, k-dungeons[i][1], dungeons);
+                dfs(dungeons, k - dungeons[i][1], depth+1);
                 visited[i] = false;
             }
         }
