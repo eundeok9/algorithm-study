a5324579d1c49ef68bd7b65f9f907f4e5a8b35d0
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2024-10-12 16:26:50 +0900

    [level 2] Title: 모음 사전, Time: 18.47 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -2,18 +2,19 @@ import java.util.*;
 
 class Solution {
     static List<String> list;
-    static String[] moeum = {"A", "E", "I", "O", "U"};
     
     public int solution(String word) {
         int answer = 0;
         list = new ArrayList<>();
+        
         dfs("",0);
-        int size = list.size();
-        for(int i=0; i<size; i++) {
+        
+        for(int i=0; i<list.size(); i++) {
             if(list.get(i).equals(word)) {
                 return i;                
             }
         }
+        
         return answer;
     }
     
@@ -21,7 +22,7 @@ class Solution {
         list.add(s);
         if(order == 5) return;
         for(int i=0; i<5; i++) {
-            dfs(s+moeum[i], order+1);
+            dfs(s+"AEIOU".charAt(i), order+1);
         }
     }
 }
