53e6e145c1fb01ef5d1180d7ab86d84956316eab
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2024-10-17 00:30:05 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -2,6 +2,7 @@
 select item_id, item_name, rarity
 from item_info
 where item_id in (
+    -- a가 parent, b는 child
     select b.item_id from item_info a, item_tree b
     where a.item_id = b.parent_item_id and a.rarity = 'RARE'
 )
