7d541994b4c08020fc244f77055e16a2e1a02af8
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2024-11-07 15:23:24 +0900

    [level 2] Title: 짝지어 제거하기, Time: 111.12 ms, Memory: 82.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution
+{
+    public int solution(String s) {
+        char[] arr = s.toCharArray();
+        Stack<Character> stack = new Stack<>();
+        
+        for(int i=0; i<arr.length; i++) {
+            char c = arr[i];
+            
+            if(stack.isEmpty()) {
+                stack.push(c);
+            } else {
+                if(stack.peek() == c) {
+                    stack.pop();
+                } else {
+                    stack.push(c);
+                }
+            }
+        }
+        
+        return stack.empty() ? 1 : 0;
+    }
+}
