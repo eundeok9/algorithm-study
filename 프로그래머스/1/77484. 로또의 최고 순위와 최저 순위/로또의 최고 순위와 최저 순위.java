c5ca9adc54fcead8d1a41b393181e695dced3a66
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2024-11-15 16:40:17 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.03 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] solution(int[] lottos, int[] win_nums) {
+        int[] answer = new int[2];
+        int[] rank = {6, 6, 5, 4, 3, 2, 1}; // 번호 일치 횟수에 따른 순위
+        
+        for(int num: lottos) {
+            if(num == 0) {
+                answer[0]++;
+                continue;
+            }
+            
+            for(int win_num: win_nums) {
+                if(num == win_num) {
+                    answer[0]++;
+                    answer[1]++;
+                }
+            }
+            
+        }
+        
+        answer[0] = rank[answer[0]];
+        answer[1] = rank[answer[1]];
+        
+        return answer;
+    }
+}
