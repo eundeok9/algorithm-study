55d16af4afb6db87f9139d877548058330a87f10
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2024-11-19 23:04:13 +0900

    [level 1] Title: 카드 뭉치, Time: 0.04 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -1,18 +1,20 @@
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        int cardIdx1 = 0;
-        int cardIdx2 = 0;
+        String answer = "Yes";
+        
+        int index1 = 0;
+        int index2 = 0;
         
         for(int i=0; i<goal.length; i++) {
-            if(cardIdx1 < cards1.length && cards1[cardIdx1].equals(goal[i])) {
-                cardIdx1++;
-            } else if(cardIdx2 < cards2.length && cards2[cardIdx2].equals(goal[i])) {
-                cardIdx2++;
+            if(index1 < cards1.length && goal[i].equals(cards1[index1])) {
+                index1++;
+            } else if(index2 < cards2.length && goal[i].equals(cards2[index2])) {
+                index2++;
             } else {
                 return "No";
             }
         }
         
-        return "Yes";
+        return answer;
     }
 }
