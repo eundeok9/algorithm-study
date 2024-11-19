df32bcda7e4680da5a21940c47eb93a5feb4e1fa
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2024-11-19 22:42:02 +0900

    [level 1] Title: 덧칠하기, Time: 2.82 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,19 +1,18 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
         int answer = 1;
-        
         int start = section[0];
-        int end = start + m - 1;
+        int end = section[0] + m - 1;
         
-        for(int i=0; i<section.length; i++) {
+        for(int i=1; i<section.length; i++) {
             if(start <= section[i] && section[i] <= end) {
                 continue;
             } else {
                 start = section[i];
-                end = start + m - 1;
+                end = section[i] + m - 1;
                 answer++;
             }
-        }     
+        }
         
         return answer;
     }
