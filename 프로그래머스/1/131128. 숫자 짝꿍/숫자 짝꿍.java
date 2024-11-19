8f9342fa8f15b5401e3174d1e1404a800d66292b
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2024-11-20 00:01:10 +0900

    Update 숫자 짝꿍.java

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -1,26 +1,33 @@
 class Solution {
     public String solution(String X, String Y) {
         StringBuilder answer = new StringBuilder();
-        int[] x = {0,0,0,0,0,0,0,0,0,0};
-        int[] y = {0,0,0,0,0,0,0,0,0,0};
-        for(int i=0; i<X.length();i++){
-           x[X.charAt(i)-48] += 1;
+        int[] count1 = new int[10];
+        int[] count2 = new int[10];
+        
+        for(int i=0; i<X.length(); i++) {
+            int num = X.charAt(i) - '0';
+            count1[num]++;
         }
-        for(int i=0; i<Y.length();i++){
-           y[Y.charAt(i)-48] += 1;
+        
+        
+        for(int i=0; i<Y.length(); i++) {
+            int num = (int)Y.charAt(i) - '0';
+            count2[num]++;
         }
 
-        for(int i=9; i >= 0; i--){
-            for(int j=0; j<Math.min(x[i],y[i]); j++){
+        
+        for(int i=9; i>=0; i--) {
+            for(int j=0; j<Math.min(count1[i], count2[i]); j++) {
                 answer.append(i);
             }
         }
-        if("".equals(answer.toString())){
-           return "-1";
-        }else if(answer.toString().charAt(0)==48){
-           return "0";
-        }else {
-            return answer.toString();
+        
+        if(answer.toString().equals("")) {
+            return "-1";
+        } else if (answer.toString().charAt(0) == '0') {
+            return "0";
         }
+        
+        return answer.toString();
     }
 }
