daa8da5d8068818b107ed889eec1bc2657b28a19
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2024-12-02 17:35:34 +0900

    [Silver II] Title: 비슷한 단어, Time: 100 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Silver/2607. 비슷한 단어/비슷한 단어.java
+++ 백준/Silver/2607. 비슷한 단어/비슷한 단어.java
@@ -1,54 +1,55 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int answer = 0;
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
 
         // 기준 단어
         String s1 = br.readLine();
-        int[] alphabet = new int[26];
-        for (int i = 0; i < s1.length(); i++) {
-            int idx = s1.charAt(i) - 'A';
-            alphabet[idx]++;
+
+        // 기준 단어에 각 문자가 몇 번 있는지
+        HashMap<Character, Integer> map1 = new HashMap<>();
+        for(int i=0; i<s1.length(); i++) {
+            map1.put(s1.charAt(i), map1.getOrDefault(s1.charAt(i), 0) + 1);
         }
 
-        int[] check;
-        for (int i = 0; i < n - 1; i++) {
-            check = alphabet.clone();
+        // 비슷한 문자 몇 개인지 count
+        int answer = 0;
+        for(int i=0; i<n-1; i++) {
             String s2 = br.readLine();
 
-            // 단어 길이가 1 이상 차이 날 경우
-            if (Math.abs(s2.length() - s1.length()) > 1) continue;
+            // 길이가 2이상 차이날 경우
+            if(Math.abs(s1.length() - s2.length()) > 1) {
+                continue;
+            }
 
-            // 같은 알파벳 개수 체크
+            HashMap<Character, Integer> map2 = new HashMap<>(map1);
             int cnt = 0;
-            for (int j = 0; j < s2.length(); j++) {
-                int idx = s2.charAt(j) - 'A';
-
-                if (check[idx] > 0) {
-                    cnt++;
-                    check[idx]--;
+            for(int j=0; j<s2.length(); j++) {
+                char c = s2.charAt(j);
+                if(map2.containsKey(c)) {
+                    if (map2.get(c) > 0) {
+                        map2.put(c, map2.get(c) - 1);
+                        cnt++;
+                    }
                 }
             }
 
-            if (s1.length() - 1 == s2.length()) { // 기준 단어보다 길이가 1 작을 경우
-                if (cnt == s2.length()) answer++;
-            } else if (s1.length() + 1 == s2.length()) {// 기준 단어보다 길이가 1 클 경우
-                if (cnt == s1.length()) answer++;
-            } else if (s1.length() == s2.length()) { // 두 단어의 길이가 같을 경우
-                // 문자 구성이 같을 때
-                if (cnt == s1.length()) answer++;
-                // 한글자만 다를 때
-                else if (cnt == s2.length() - 1) answer++;
+            if(s1.length() -1 == s2.length()) {
+                if(cnt == s2.length()) answer++;
+            } else if(s1.length() + 1 == s2.length()) {
+                if(cnt == s1.length()) answer++;
+            } else if(s1.length() == s2.length()) {
+                if(cnt == s1.length()) answer++;
+                else if(cnt == s2.length() - 1) answer++;
             }
-
         }
+
         System.out.println(answer);
     }
 }
