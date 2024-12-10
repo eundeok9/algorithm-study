57eea4d5006bd8ecdc736ea76e17c514a19447a0
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2024-12-10 23:22:24 +0900

    [Gold IV] Title: 빙산, Time: 496 ms, Memory: 194160 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.java
+++ 백준/Gold/2573. 빙산/빙산.java
@@ -6,104 +6,103 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static BufferedReader br;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    static int n, m;
+
+    static int N, M;
     static int[][] map;
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, -1, 0, 1};
     static boolean[][] visited;
 
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
     public static void main(String[] args) throws IOException {
-        // 입력값 처리
         init();
 
         int year = 0;
         while(true) {
-            int count = 0; // 섬으로 연결된 영역의 개수
-            visited = new boolean[n][m];
+            int count = 0;
+            visited = new boolean[N][M];
 
-            for(int i=0; i<n; i++) {
-                for(int j=0; j<m; j++) {
-                    if(map[i][j] > 0 && !visited[i][j]) {
-                        bfs(i, j); // 영역 찾기
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    if (map[i][j] != 0 && !visited[i][j]) {
+                        bfs(i, j);
                         count++;
                     }
                 }
             }
 
-            if(count == 0) { // 영역이 만들어지지 않을 경우
-                System.out.println(0); // 0 출력
+            if (count == 0) {
+                System.out.println(0);
                 return;
-            }
-            if(count > 1) { // 영역이 2개 이상이 되는 경우
-                System.out.println(year); // 년 출력
+            } else if (count >= 2) {
+                System.out.println(year);
                 return;
             }
 
-            // 영역이 1개인 경우, 빙산 녹이고 1년 추가
             melting();
             year++;
         }
     }
 
+    public static void melting() {
+        int[][] newMap = new int[N][M];
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<M; j++) {
+                if(map[i][j] != 0) {
+                    int cnt = 0;
+                    for (int d = 0; d < 4; d++) {
+                        int nx = i + dx[d];
+                        int ny = j + dy[d];
+                        if (check(nx, ny)) continue;
+                        if (map[nx][ny] == 0) cnt++;
+                    }
+                    newMap[i][j] = Math.max((map[i][j] - cnt), 0);
+                }
+            }
+        }
+
+        map = newMap;
+    }
+    public static void bfs(int i, int j) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[] {i, j});
+        visited[i][j] = true;
+
+        while(!queue.isEmpty()) {
+            int[] node = queue.poll();
+            int x = node[0];
+            int y = node[1];
+            for(int d=0; d<4; d++) {
+                int nx = x + dx[d];
+                int ny = y + dy[d];
+                if(check(nx, ny)) continue;
+                if(map[nx][ny] != 0 && !visited[nx][ny]) {
+                    queue.add(new int[] {nx, ny});
+                    visited[nx][ny] = true;
+                }
+            }
+        }
+
+    }
+
+    public static boolean check(int x, int y) {
+        return 0 > x || x >= N || 0 > y || y >= M;
+    }
+
     public static void init() throws IOException {
-        br = new BufferedReader(new InputStreamReader(System.in));
         st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        map = new int[n][m];
-
-        for(int i=0; i<n; i++) {
+        map = new int[N][M];
+        for(int i=0; i<N; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j=0; j<m; j++) {
+            for(int j=0; j<M; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
     }
 
-    // 섬의 영역 개수 세기
-    public static void bfs(int x, int y) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[] {x, y});
-        visited[x][y] = true;
-
-        while(!queue.isEmpty()) {
-            int[] cur = queue.poll();
-            for(int d=0; d<4; d++) {
-                int nx = cur[0] + dx[d];
-                int ny = cur[1] + dy[d];
-                if(check(nx, ny) && !visited[nx][ny] && map[nx][ny] > 0) {
-                    visited[nx][ny] = true;
-                    queue.add(new int[] {nx, ny});
-                }
-            }
-        }
-    }
-
-    public static void melting() {
-        int[][] newMap = new int[n][m];
-
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                if(map[i][j] > 0) {
-                    int water = 0;
-                    for(int d=0; d<4; d++) {
-                        int nx = i + dx[d];
-                        int ny = j + dy[d];
-                        if(check(nx, ny) && map[nx][ny] == 0) {
-                            water++;
-                        }
-                    }
-                    newMap[i][j] = Math.max(0, map[i][j] - water);
-                }
-            }
-        }
-        map = newMap;
-    }
-
-    public static boolean check(int x, int y) {
-        return 0 <= x && x < n && 0 <= y && y < m;
-    }
 }
