0e6a1be45aa580c24c5d3c643791f911b9443fb0
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2024-12-12 22:21:08 +0900

    [Silver III] Title: 타노스, Time: 108 ms, Memory: 14452 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.java
+++ 백준/Silver/20310. 타노스/타노스.java
@@ -15,14 +15,29 @@ public class Main {
             }
         }
 
-        StringBuilder sb = new StringBuilder();
-        for(int i=0; i<zeroCnt/2; i++) {
-            sb.append(0);
-        }
-        for(int i=0; i<oneCnt/2; i++) {
-            sb.append(1);
+        zeroCnt /= 2;
+        oneCnt /= 2;
+
+        int i = 0;
+        while(oneCnt != 0) {
+            if(s.charAt(i) == '1') {
+                s = s.substring(0, i) + s.substring(i+1);
+                oneCnt--;
+                i = -1;
+            }
+            i++;
         }
 
-        System.out.println(sb);
+        int j = s.length() - 1;
+        while(zeroCnt != 0) {
+            if(s.charAt(j) == '0') {
+                s = s.substring(0, j) + s.substring(j+1);
+                zeroCnt--;
+                j = s.length();
+            }
+            j--;
+        }
+
+        System.out.println(s);
     }
 }
