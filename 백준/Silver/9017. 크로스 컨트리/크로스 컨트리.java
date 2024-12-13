cf2fe87fa3ba7a3992f8d6fcffd163d3f7f48a97
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2024-12-13 14:30:44 +0900

    [Silver III] Title: 크로스 컨트리, Time: 172 ms, Memory: 16936 KB -BaekjoonHub

--- 백준/Silver/9017. 크로스 컨트리/크로스 컨트리.java
+++ 백준/Silver/9017. 크로스 컨트리/크로스 컨트리.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
         int T = Integer.parseInt(br.readLine());
         for(int tc=0; tc<T; tc++) {
@@ -59,7 +60,8 @@ public class Main {
                     }
                 }
             }
-            System.out.println(answer);
+            sb.append(answer).append("\n");
         }
+        System.out.println(sb);
     }
 }
