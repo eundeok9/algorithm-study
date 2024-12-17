c3f6bb77198036a6905c542a0e81f6d24bdf0100
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2024-12-17 20:38:53 +0900

    [Gold III] Title: 소문난 칠공주, Time: 312 ms, Memory: 92512 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
@@ -1,37 +1,35 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
-    static char[][] map;
+    static final int N = 5; // 5*5
+    static char[][] map = new char[N][N];
+
     static int[] selected = new int[7];
     static boolean[] visited;
+
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
 
     static int answer = 0;
 
-    static final int N = 5;
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        init();
+        combination(0, 0, 0);
+        System.out.println(answer);
+    }
 
-        map = new char[N][N];
+    public static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         for(int i=0; i<N; i++) {
             map[i] = br.readLine().toCharArray();
         }
-
-        dfs(0, 0, 0);
-
-        System.out.println(answer);
-
     }
 
-    /**
-     * 조합을 구해보자
-     * @param depth : 학생 수
-     * @param yCnt : 임도연파 수
-     * @param start : 시작 index
-     */
-    private static void dfs(int depth, int yCnt, int start) {
+    public static void combination(int depth, int yCnt, int start) {
         if(yCnt >= 4) return;
 
         if(depth == 7) {
@@ -43,19 +41,19 @@ public class Main {
         for(int i=start; i<25; i++) {
             selected[depth] = i;
             if(map[i/5][i%5] == 'Y') {
-                dfs(depth + 1, yCnt + 1, i+1);
+                combination(depth + 1, yCnt+1, i + 1);
             } else {
-                dfs(depth + 1, yCnt, i+1);
+                combination(depth + 1, yCnt, i+1);
             }
         }
     }
 
-    private static void bfs() {
+    public static void bfs() {
+        int count = 1;
         Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[] {selected[0]/ 5, selected[0] % 5});
+        queue.add(new int[] {selected[0] / 5, selected[0] % 5});
         visited[0] = true;
 
-        int count = 1;
         while(!queue.isEmpty()) {
             int[] cur = queue.poll();
             int x = cur[0];
@@ -65,22 +63,20 @@ public class Main {
                 int nx = x + dx[d];
                 int ny = y + dy[d];
                 int ni = nx * 5 + ny;
-
                 if(check(nx, ny)) continue;
-                for(int j=0; j<7; j++) {
-                    if(!visited[j] && selected[j] == ni) {
+                for(int i=0; i<7; i++) {
+                    if(!visited[i] && selected[i] == ni) {
                         queue.add(new int[] {nx, ny});
-                        visited[j] = true;
+                        visited[i] = true;
                         count++;
                     }
                 }
             }
         }
-
         if(count == 7) answer++;
     }
 
-    private static boolean check(int x, int y) {
+    public static boolean check(int x, int y) {
         return 0 > x || x >= N || 0 > y || y >= N;
     }
 }
