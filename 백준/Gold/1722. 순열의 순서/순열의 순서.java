ce3f0d50a1f28dcf7e302799b5e2f0b76567f8ff
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2024-12-19 18:35:35 +0900

    [Gold V] Title: 순열의 순서, Time: 100 ms, Memory: 14352 KB -BaekjoonHub

--- 백준/Gold/1722. 순열의 순서/순열의 순서.java
+++ 백준/Gold/1722. 순열의 순서/순열의 순서.java
@@ -2,42 +2,44 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int N;
+    static StringBuilder sb = new StringBuilder();
+    static long[] factorial;
+    static boolean[] visited;
+
     public static void main(String[] args) throws IOException {
-        int N;
-        long[] factorial;
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
-
         N = Integer.parseInt(br.readLine());
 
         factorial = new long[N+1];
-        // 팩토리얼 배열 (각 자리수에서 만들 수 있는 경우의 수)
         factorial[0] = 1;
         for(int i=1; i<=N; i++) {
             factorial[i] = factorial[i-1] * i;
         }
 
-        st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int num = Integer.parseInt(st.nextToken());
-        boolean[] used = new boolean[N+1];
+        visited = new boolean[N+1];
+
+        // 1번 문제
         if(num == 1) {
             long k = Long.parseLong(st.nextToken());
             for(int i=0; i<N; i++) {
                 for(int j=1; j<=N; j++) {
-                    if(used[j]) continue;
+                    if(visited[j]) continue;
 
-                    if(k <= factorial[N-i-1]) {
+                    if(k <= factorial[N-1-i]) {
                         sb.append(j).append(" ");
-                        used[j] = true;
+                        visited[j] = true;
                         break;
                     } else {
                         k -= factorial[N-i-1];
                     }
                 }
             }
-        } else {
+        }
+        // 2번 문제
+        else {
             int[] goal = new int[N];
             for(int i=0; i<N; i++) {
                 goal[i] = Integer.parseInt(st.nextToken());
@@ -46,14 +48,14 @@ public class Main {
             long answer = 1;
             for(int i=0; i<N; i++) {
                 for(int j=1; j<goal[i]; j++) {
-                    if(used[j]) continue;
-                    answer += factorial[N-1-i];
+                    if(visited[j]) continue;
+
+                    answer += factorial[N-i-1];
                 }
-                used[goal[i]] = true;
+                visited[goal[i]] = true;
             }
             sb.append(answer);
         }
-
         System.out.println(sb);
     }
 }
