41ab19ac12171921fdabd81d7474336f87d6b1ea
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2024-12-20 14:20:43 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 29.23 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/README.md
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/README.md
@@ -1,10 +1,10 @@
 # [level 2] 메뉴 리뉴얼 - 72411 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72411) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72411#) 
 
 ### 성능 요약
 
-메모리: 106 MB, 시간: 28.89 ms
+메모리: 78.6 MB, 시간: 29.23 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 18일 16:20:35
+2024년 12월 20일 14:20:40
 
 ### 문제 설명
 
--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -1,47 +1,47 @@
 import java.util.*;
 
 class Solution {
-    static ArrayList<String> answer = new ArrayList<>();
-    static HashMap<String, Integer> map = new HashMap<>();
+    static HashMap<String, Integer> map;
     public String[] solution(String[] orders, int[] course) {
-        // orders 오름차순 정렬
+        ArrayList<String> answer = new ArrayList<>();
+        
         for(int i=0; i<orders.length; i++) {
-            char[] arr = orders[i].toCharArray();
-            Arrays.sort(arr);
-            orders[i] = String.valueOf(arr);
+            char[] carr = orders[i].toCharArray();
+            Arrays.sort(carr);
+            orders[i] = String.valueOf(carr);
         }
         
         for(int num: course) {
+            map = new HashMap<>();
             for(String order: orders) {
-                combi("", order, num, 0, 0);
+                combination(0, 0, num, "", order);
             }
             
             if(!map.isEmpty()) {
-                List<Integer> count = new ArrayList<>(map.values());
-                int max = Collections.max(count);
-                if(max < 2) continue;
-                for(String key: map.keySet()) {
-                    if(max == map.get(key)) {
-                        answer.add(key);
+                ArrayList<Integer> list = new ArrayList<>(map.values());
+                int max = Collections.max(list);
+                if(max >= 2) {
+                    for(String key: map.keySet()) {
+                        if(map.get(key) == max) {
+                            answer.add(key);
+                        }
                     }
                 }
             }
-            map.clear();
         }
-        
         Collections.sort(answer);
         
         return answer.toArray(new String[answer.size()]);
     }
     
-    public static void combi(String s, String order, int num, int depth, int start) {
-        if(num == depth) {
+    public static void combination(int depth, int start, int num, String s, String order) {
+        if(depth == num) {
             map.put(s, map.getOrDefault(s, 0) + 1);
             return;
         }
         
         for(int i=start; i<order.length(); i++) {
-            combi(s + order.charAt(i), order, num, depth+1, i+1);
+            combination(depth + 1, i + 1, num, s + order.charAt(i), order);
         }
     }
 }
