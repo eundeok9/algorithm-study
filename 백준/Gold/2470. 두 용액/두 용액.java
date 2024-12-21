5397867a7f57d928ca33ee66673aaafd4489fbc9
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2024-12-21 16:07:03 +0900

    [Gold V] Title: 두 용액, Time: 352 ms, Memory: 29172 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long[] nums = new long[N];
+        for(int i=0; i<N; i++) {
+            nums[i] = Long.parseLong(st.nextToken());
+        }
+        Arrays.sort(nums);
+
+        int left = 0;
+        int right = N-1;
+        long minDiff = Long.MAX_VALUE;
+        long[] answer = new long[2];
+        while(left<right) {
+            long sum = nums[left] + nums[right];
+
+            if(minDiff > Math.abs(sum)) {
+                minDiff = Math.abs(sum);
+                answer[0] = nums[left];
+                answer[1] = nums[right];
+            }
+
+            if(sum < 0) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        System.out.println(answer[0] + " " + answer[1]);
+    }
+}
