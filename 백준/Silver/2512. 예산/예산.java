675636d360ea204de696cf69a6bfcd01562f5f48
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2024-12-23 15:57:04 +0900

    [Silver II] Title: 예산, Time: 136 ms, Memory: 15240 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.java
+++ 백준/Silver/2512. 예산/예산.java
@@ -3,33 +3,33 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-           BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-           
-           int N = Integer.parseInt(br.readLine());
-           
-           int left = 0;
-           int right = -1;
-           int[] arr = new int[N];
-           StringTokenizer st = new StringTokenizer(br.readLine());
-           for(int i=0; i<N; i++) {
-               arr[i] = Integer.parseInt(st.nextToken());
-               right = Math.max(right, arr[i]);
-           }
-           
-           int m = Integer.parseInt(br.readLine());
-           while(left <= right) {
-               int mid = (left+right) / 2;
-               long money = 0;
-               for(int i=0; i<N; i++) {
-                   if(arr[i] > mid) money += mid;
-                   else money += arr[i];
-               }
-               if(money <= m) {
-                   left = mid + 1;
-               } else {
-                   right = mid -  1;
-               }
-           }
-           System.out.println(right);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        int left = 0;
+        int right = 0;
+        for(int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            right = Math.max(right, arr[i]);
+        }
+
+        long M = Integer.parseInt(br.readLine());
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            long money = 0;
+            for(int i=0; i<N; i++) {
+                if(mid < arr[i]) money += mid;
+                else money += arr[i];
+            }
+            if(money > M) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        System.out.println(right);
     }
 }
