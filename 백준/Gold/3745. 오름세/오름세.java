57f9e3f8c1aa4ea9d9bd7d97059b1857ce01196c
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2025-01-02 18:19:42 +0900

    [Gold II] Title: 오름세, Time: 272 ms, Memory: 27096 KB -BaekjoonHub

---
+++ 백준/Gold/3745. 오름세/오름세.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        String input = null;
+        while((input = br.readLine()) != null) {
+            int N = Integer.parseInt(input.trim());
+            int[] arr = new int[N];
+            st = new StringTokenizer(br.readLine());
+            for(int i=0; i<N; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+
+            int[] lis = new int[N];
+            lis[0] = arr[0];
+            int len = 1;
+            for(int i=1; i<N; i++) {
+                if(lis[len-1] < arr[i]) {
+                    lis[len++] = arr[i];
+                } else {
+                    int idx = binarySearch(0, len-1, arr[i], lis);
+                    lis[idx] = arr[i];
+                }
+            }
+            System.out.println(len);
+        }
+    }
+
+    public static int binarySearch(int left, int right, int target, int[] lis) {
+        int mid;
+
+        while(left < right) {
+            mid = (left + right) / 2;
+
+            if(lis[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return right;
+    }
+}
