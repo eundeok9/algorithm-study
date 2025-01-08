0d49199d6d7210ac52a8e4afc215b692c382b68b
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2025-01-08 23:15:02 +0900

    [Gold I] Title: K번째 수, Time: 140 ms, Memory: 14296 KB -BaekjoonHub

---
+++ 백준/Gold/1300. K번째 수/K번째 수.java
@@ -0,0 +1,29 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int K = Integer.parseInt(br.readLine());
+
+        long left = 1;
+        long right = K;
+        
+        while(left < right) {
+            long mid = (left + right) / 2;
+            long count = 0;
+            
+            for(int i=1; i<=N; i++) {
+                count += Math.min(mid/i, N);
+            }
+            
+            if(K<=count) {
+                right = mid;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        System.out.println(left);
+    }
+}
