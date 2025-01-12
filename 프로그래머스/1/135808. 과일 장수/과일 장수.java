43d51125e4b0d3ca518bcff9d11db232e523cf97
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2025-01-12 14:46:21 +0900

    [level 1] Title: 과일 장수, Time: 80.51 ms, Memory: 154 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -4,9 +4,9 @@ class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
         int box = score.length / m;
-    
+        
         Arrays.sort(score);
-
+        
         for(int i=1; i<=box; i++) {
             answer += score[score.length - m * i] * m;
         }
