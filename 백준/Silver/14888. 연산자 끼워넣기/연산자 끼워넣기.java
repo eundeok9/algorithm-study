04fa10ed698c1216c97965c7ae4ffae338e8c523
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2025-01-12 22:39:05 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 132 ms, Memory: 16264 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -6,7 +6,6 @@ public class Main {
     static int[] operationCnt = new int[4];
     static int maxValue = Integer.MIN_VALUE;
     static int minValue = Integer.MAX_VALUE;
-    static List<Integer> operation = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -25,52 +24,38 @@ public class Main {
             operationCnt[i] = Integer.parseInt(st.nextToken());
         }
 
-        backTracking(0);
+        backTracking(0, num[0]);
         System.out.println(maxValue + "\n" + minValue);
     }
 
-    public static void backTracking(int depth) {
+    public static void backTracking(int depth, int currentValue) {
         if(depth == N-1) {
-            // 계산
-            int res = num[0];
-            for(int i=1; i<N; i++) {
-                int op = operation.get(i-1);
-
-                switch (op) {
-                    case 0: // 덧셈;
-                        res += num[i];
-                        break;
-                    case 1: // 뺄셈
-                        res -= num[i];
-                        break;
-                    case 2: // 곱셈
-                        res *= num[i];
-                        break;
-                    case 3: // 나눗셈
-                        if (num[i] == 0) continue;
-                        if (res < 0 || num[i] < 0) { // 둘 중에 하나가 음수면
-                            int tmp = Math.abs(res) / Math.abs(num[i]);
-                            res = -1 * tmp;
-                        } else {
-                            res /= num[i];
-                        }
-                        break;
-                }
-            }
-
-            minValue = Math.min(res, minValue);
-            maxValue = Math.max(res, maxValue);
+            minValue = Math.min(currentValue, minValue);
+            maxValue = Math.max(currentValue, maxValue);
 
             return;
         }
 
         for(int i=0; i<4; i++) {
-            if(operationCnt[i] != 0) {
+            if(operationCnt[i] > 0) {
                 operationCnt[i]--;
-                operation.add(i);
-                backTracking(depth + 1);
+                int newValue = currentValue;
+
+                switch(i) {
+                    case 0: newValue += num[depth + 1]; break;
+                    case 1: newValue -= num[depth + 1]; break;
+                    case 2: newValue *= num[depth + 1]; break;
+                    case 3:
+                        if(newValue < 0) {
+                            newValue = -1 * (Math.abs(newValue) / num[depth + 1]);
+                        } else {
+                            newValue /= num[depth + 1];
+                        }
+                        break;
+                }
+                backTracking(depth + 1, newValue);
                 operationCnt[i]++;
-                operation.remove(operation.size() - 1);
+
             }
         }
     }
