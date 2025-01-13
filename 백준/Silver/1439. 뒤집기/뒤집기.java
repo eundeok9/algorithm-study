800b045e487e9c334d1b454a62ec7f68433ab3b6
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2025-01-13 23:47:53 +0900

    [Silver V] Title: 뒤집기, Time: 104 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Silver/1439. 뒤집기/뒤집기.java
@@ -0,0 +1,27 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        int zero = 0;
+        int one = 0;
+
+        char ch = s.charAt(0);
+        if(ch=='0') {
+            zero++;
+        } else {
+            one++;
+        }
+
+        for(int i=1; i<s.length(); i++) {
+            if(s.charAt(i) != ch) {
+                ch = s.charAt(i);
+                if(s.charAt(i) == '1') one++;
+                else zero++;
+            }
+        }
+
+        System.out.println(Math.min(zero, one));
+    }
+}
