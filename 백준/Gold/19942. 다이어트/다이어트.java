dd0141231f3fb7913078f55b5a4ac2e721e47c9a
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2025-01-15 20:28:58 +0900

    [Gold IV] Title: 다이어트, Time: 148 ms, Memory: 17968 KB -BaekjoonHub

---
+++ 백준/Gold/19942. 다이어트/다이어트.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    static int N;
+    static int mp, mf, ms, mv;
+    static int[][] gradient;
+    static int minPrice = Integer.MAX_VALUE;
+    static ArrayList<Integer> answer = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        N = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+        mp = Integer.parseInt(st.nextToken());
+        mf = Integer.parseInt(st.nextToken());
+        ms = Integer.parseInt(st.nextToken());
+        mv = Integer.parseInt(st.nextToken());
+
+        gradient = new int[N][5];
+        for(int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<5; j++) {
+                gradient[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        backTracking(0, 0, 0, 0, 0, 0, new ArrayList<>());
+        if(minPrice == Integer.MAX_VALUE) {
+            System.out.println(-1);
+            return;
+        }
+        System.out.println(minPrice);
+        for(int i: answer) {
+            System.out.print((i+1) + " ");
+        }
+    }
+
+    public static void backTracking(int start, int p, int f, int s, int v, int price, ArrayList<Integer> list) {
+        if(price >= minPrice) return;
+
+        if(p >= mp && f >= mf && s >= ms && v >= mv) {
+            minPrice = price;
+            answer = new ArrayList<>(list);
+            return;
+        }
+
+        for(int i=start; i<N; i++) {
+            int[] cur = gradient[i];
+            list.add(i);
+            backTracking(i+1, p + cur[0], f + cur[1], s + cur[2], v + cur[3], price + cur[4], list);
+            list.remove(list.size() - 1);
+        }
+    }
+}
