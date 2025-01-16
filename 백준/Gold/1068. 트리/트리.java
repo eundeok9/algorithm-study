ef425e9fcd9617893ede6b5d20a6c89b66dcd82e
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2025-01-16 15:02:56 +0900

    [Gold V] Title: 트리, Time: 108 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Gold/1068. 트리/트리.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    static List<List<Integer>> graph;
+    static int answer = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        graph = new ArrayList<>();
+        for(int i=0; i<N; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        int root = -1;
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+            int parent = Integer.parseInt(st.nextToken());
+            if(parent != -1) {
+                graph.get(parent).add(i);
+            } else {
+                root = i;
+            }
+        }
+
+        int delNode = Integer.parseInt(br.readLine());
+        if(delNode != root) {
+            dfs(root, delNode);
+        }
+
+        System.out.println(answer);
+    }
+
+    public static void dfs(int current, int delNode) {
+        if(current == delNode) {
+            return; // 삭제된 노드는 탐색X
+        }
+
+        boolean isLeaf = true;
+        for(int child: graph.get(current)) {
+            if(child != delNode) {
+                isLeaf = false;
+                dfs(child, delNode);
+            }
+        }
+
+        if(isLeaf) {
+            answer++;
+        }
+    }
+}
