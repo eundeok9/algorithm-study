1d1cde78014924f95772c5f3d4db68e73652e323
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2025-01-17 14:29:57 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 580 ms, Memory: 69816 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -8,38 +8,37 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
 
-        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-        for(int i=0; i<=N; i++) {
+        List<List<Integer>> graph = new ArrayList<>();
+        for(int i=0; i<N; i++) {
             graph.add(new ArrayList<>());
         }
-
         for(int i=0; i<N-1; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken()) - 1;
+            int b = Integer.parseInt(st.nextToken()) - 1;
             graph.get(a).add(b);
             graph.get(b).add(a);
         }
 
-        int[] parent = new int[N+1];
-        boolean[] visited = new boolean[N+1];
-
+        int[] parent = new int[N];
+        boolean[] visited = new boolean[N];
         Queue<Integer> queue = new LinkedList<>();
-        queue.add(1);
-        visited[1] = true;
+        queue.add(0);
+        visited[0] = true;
 
         while(!queue.isEmpty()) {
-            int current = queue.poll();
-            for(int next : graph.get(current)) {
-                if(!visited[next]) {
-                    visited[next] = true;
-                    parent[next] = current;
-                    queue.add(next);
+            int cur = queue.poll();
+
+            for(int nxt: graph.get(cur)) {
+                if(!visited[nxt]) {
+                    parent[nxt] = cur+1;
+                    visited[nxt] = true;
+                    queue.add(nxt);
                 }
             }
         }
 
-        for(int i=2; i<=N; i++) {
+        for(int i=1; i<N; i++) {
             sb.append(parent[i]).append("\n");
         }
         System.out.println(sb);
