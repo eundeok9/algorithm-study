39719ec18c8234d1e42f150d7ceda93ec375f34d
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2025-01-21 00:13:57 +0900

    [Gold V] Title: 트리, Time: 104 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.java
+++ 백준/Gold/1068. 트리/트리.java
@@ -1,22 +1,23 @@
 import java.io.*;
 import java.util.*;
 public class Main {
+    static int N;
     static List<List<Integer>> graph;
-    static int delNode;
     static int answer = 0;
+    static boolean[] visited;
+    static int delNode;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        int N = Integer.parseInt(br.readLine());
-
+        N = Integer.parseInt(br.readLine());
         graph = new ArrayList<>();
         for(int i=0; i<N; i++) {
             graph.add(new ArrayList<>());
         }
 
-        int root = -1;
         st = new StringTokenizer(br.readLine());
+        int root = -1;
         for(int i=0; i<N; i++) {
             int parent = Integer.parseInt(st.nextToken());
             if(parent != -1) {
@@ -26,6 +27,7 @@ public class Main {
             }
         }
 
+        visited = new boolean[N];
         delNode = Integer.parseInt(br.readLine());
         if(delNode != root) {
             dfs(root);
@@ -34,16 +36,15 @@ public class Main {
         System.out.println(answer);
     }
 
-    public static void dfs(int current) {
-        if(current == delNode) {
-            return; // 삭제된 노드는 탐색X
-        }
+    public static void dfs(int cur) {
+        visited[cur] = true;
 
         boolean isLeaf = true;
-        for(int child: graph.get(current)) {
-            if(child != delNode) {
+        for(int nxt: graph.get(cur)) {
+            if(!visited[nxt] && nxt != delNode) {
                 isLeaf = false;
-                dfs(child);
+                visited[nxt] = true;
+                dfs(nxt);
             }
         }
 
