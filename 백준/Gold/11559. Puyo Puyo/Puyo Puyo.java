1d40a44cd089a1fce1a6b400dfdddc3ced9b84cf
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2025-02-03 15:57:43 +0900

    [Gold IV] Title: Puyo Puyo, Time: 104 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Gold/11559. Puyo Puyo/Puyo Puyo.java
+++ 백준/Gold/11559. Puyo Puyo/Puyo Puyo.java
@@ -1,89 +1,102 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-    static char[][] map;
-    static boolean[][] visited;
+    static final int N = 12;
+    static final int M = 6;
+    static char[][] map = new char[N][M];
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
+    static boolean[][] visited;
+    static ArrayList<int[]> puyoGroup;
     static int answer = 0;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        map = new char[12][6];
-        for(int i=0; i<12; i++) {
+        for(int i=0; i<N; i++) {
             map[i] = br.readLine().toCharArray();
         }
 
         while(true) {
-            visited = new boolean[12][6];
-            boolean isPopped = false; // 한 번이라도 터졌는지
+            visited = new boolean[N][M];
+            boolean isPuyo = false;
 
-            for(int i=0; i<12; i++) {
-                for(int j=0; j<6; j++) {
+            for(int i=0; i<N; i++) {
+                for(int j=0; j<M; j++) {
                     if(map[i][j] != '.' && !visited[i][j]) {
                         if(bfs(i, j)) {
-                            isPopped = true;
+                            isPuyo = true;
                         }
                     }
                 }
             }
 
-            if(!isPopped) break;
+            if(!isPuyo) {
+                break;
+            }
 
-            gravity();
             answer++;
+
+            // 연쇄 발생해서 빈칸이 생긴 곳 채워주기
+            gravity();
         }
         System.out.println(answer);
     }
 
     public static boolean bfs(int i, int j) {
         Queue<int[]> queue = new LinkedList<>();
-        List<int[]> puyoGroup = new ArrayList<>(); // 터질 뿌요 좌표 저장
-
         queue.add(new int[] {i, j});
-        puyoGroup.add(new int[] {i, j});
         visited[i][j] = true;
-        char color = map[i][j]; // 현재 뿌요 색
+
+        puyoGroup = new ArrayList<>();
+        puyoGroup.add(new int[] {i, j});
 
         while(!queue.isEmpty()) {
             int[] cur = queue.poll();
+            int x = cur[0];
+            int y = cur[1];
 
             for(int d=0; d<4; d++) {
-                int nx = cur[0] + dx[d];
-                int ny = cur[1] + dy[d];
-
-                if(nx < 0 || nx >= 12 || ny < 0 || ny >= 6) continue;
-                if(visited[nx][ny] || map[nx][ny] != color) continue;
-
-                queue.add(new int[] {nx, ny});
-                puyoGroup.add(new int[] {nx, ny});
-                visited[nx][ny] = true;
+                int nx = x + dx[d];
+                int ny = y + dy[d];
+                if(check(nx, ny) && !visited[nx][ny]) {
+                    if(map[nx][ny] == map[i][j]) {
+                        visited[nx][ny] = true;
+                        puyoGroup.add(new int[] {nx, ny});
+                        queue.add(new int[] {nx, ny});
+                    }
+                }
             }
         }
 
-        if(puyoGroup.size() >= 4) {
-            for(int[] p : puyoGroup) {
-                map[p[0]][p[1]] = '.';
-            }
-            return true;
+        if(puyoGroup.size() < 4) {
+            return false;
         }
 
-        return false;
+        for(int[] puyo: puyoGroup) {
+            map[puyo[0]][puyo[1]] = '.';
+        }
+
+        return true;
     }
 
     public static void gravity() {
-        for(int j=0; j<6; j++) {
-            for(int i=11; i>=0; i--) {
+        for(int j=0; j<M; j++) {
+            for(int i=N-1; i>=0; i--) {
                 if(map[i][j] == '.') {
                     int k = i - 1;
                     while(k >= 0 && map[k][j] == '.') k--;
 
-                    if(k>=0) {
+                    if(k >= 0) {
                         map[i][j] = map[k][j];
                         map[k][j] = '.';
                     }
                 }
             }
         }
+    }
+
+    public static boolean check(int i, int j) {
+        return 0 <= i && i < N && 0 <= j && j < M;
     }
 }
