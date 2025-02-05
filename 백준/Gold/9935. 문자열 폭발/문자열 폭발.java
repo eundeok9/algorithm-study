c771a3922e56f564d040b0233c92f9d1d9b2f3da
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2025-02-05 16:34:57 +0900

    [Gold IV] Title: 문자열 폭발, Time: 408 ms, Memory: 88580 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -1,44 +1,23 @@
 import java.io.*;
-import java.util.Stack;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        StringBuilder sb = new StringBuilder();
 
         String s = br.readLine();
         String bomb = br.readLine();
         int bLength = bomb.length();
 
-        Stack<Character> stack = new Stack<>();
-        for(int i=0; i<s.length(); i++) {
-            stack.push(s.charAt(i));
+        for(char ch: s.toCharArray()) {
+            sb.append(ch);
 
-            if(stack.size() >= bLength) {
-                boolean flag = true;
-
-                for(int j=0; j<bLength; j++) {
-                    if(stack.get(stack.size() - bLength + j) != bomb.charAt(j)) {
-                        flag = false;
-                        break;
-                    }
-                }
-
-                if(flag) {
-                    for(int j=0; j<bLength; j++) {
-                        stack.pop();
-                    }
+            if(sb.length() >= bLength) {
+                if(sb.substring(sb.length() - bLength).equals(bomb)) {
+                   sb.delete(sb.length() - bLength, sb.length());
                 }
             }
         }
 
-        if(stack.isEmpty()) {
-            System.out.println("FRULA");
-        } else {
-            StringBuilder sb = new StringBuilder();
-            for(char ch: stack) {
-                sb.append(ch);
-            }
-            System.out.println(sb);
-        }
+        System.out.println(sb.length() == 0 ? "FRULA" : sb.toString());
     }
 }
