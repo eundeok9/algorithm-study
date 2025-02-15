cc492880ea89ce784933cd66e20f045104330ccf
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2025-02-15 23:09:53 +0900

    [Gold IV] Title: 구슬 찾기, Time: 152 ms, Memory: 15604 KB -BaekjoonHub

--- 백준/Gold/2617. 구슬 찾기/구슬 찾기.java
+++ 백준/Gold/2617. 구슬 찾기/구슬 찾기.java
@@ -9,39 +9,36 @@ public class Main {
         int M = Integer.parseInt(st.nextToken());
 
         int[][] dist = new int[N+1][N+1];
-
         for(int i=0; i<M; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            dist[a][b] = 1;
+            dist[Integer.parseInt(st.nextToken())][Integer.parseInt(st.nextToken())] = 1;
         }
 
         for(int k=1; k<=N; k++) {
             for(int i=1; i<=N; i++) {
                 for(int j=1; j<=N; j++) {
                     if(dist[i][k] == 1 && dist[k][j] == 1) {
-                        dist[i][j] = 1; // i -> k -> j 경로가 있으면 구슬의 무게 판별 가능 (i < j)
+                        dist[i][j] = 1;
                     }
                 }
             }
         }
 
-        int mid = (N+1) / 2; // 중간 순위를 가지려면 최대 mid - 1까지만 무겁거나 가벼워야 함
+        int mid = (N+1)/2;
         int answer = 0;
-
         for(int i=1; i<=N; i++) {
-            int heavy = 0, light = 0;
+            int light = 0;
+            int heavy = 0;
+
             for(int j=1; j<=N; j++) {
-                if(dist[i][j] == 1) light++; // i보다 무거운 구술 개수
+                if(dist[i][j] == 1) light++;
                 if(dist[j][i] == 1) heavy++;
             }
 
-            if(heavy >= mid || light >= mid) {
+            if(light >= mid || heavy >= mid) {
                 answer++;
             }
         }
-
         System.out.println(answer);
     }
 }
