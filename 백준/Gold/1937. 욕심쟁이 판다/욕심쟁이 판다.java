0720c8d899db1152af0f3dca951930dfb8777c10
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2025-03-23 00:52:52 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 456 ms, Memory: 39628 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
@@ -12,9 +12,10 @@ public class Main {
 
         N = Integer.parseInt(br.readLine());
 
-        map = new int[N][N];
         dp = new int[N][N];
+        map = new int[N][N];
         for(int i=0; i<N; i++) {
+            Arrays.fill(dp[i], -1);
             st = new StringTokenizer(br.readLine());
             for(int j=0; j<N; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
@@ -24,33 +25,28 @@ public class Main {
         int answer = 0;
         for(int i=0; i<N; i++) {
             for(int j=0; j<N; j++) {
-                answer = Math.max(answer, dfs(i, j));
+                answer = Math.max(answer ,dfs(i, j));
             }
         }
+
         System.out.println(answer);
     }
 
     public static int dfs(int x, int y) {
-        if(dp[x][y] != 0) {
-            return dp[x][y];
-        }
+        if(dp[x][y] != -1) return dp[x][y];
 
         dp[x][y] = 1;
         for(int d=0; d<4; d++) {
             int nx = x + dx[d];
             int ny = y + dy[d];
 
-            if(!check(nx, ny)) continue;
-            if(map[x][y] < map[nx][ny]) {
-                dp[x][y] = Math.max(dp[x][y], dfs(nx, ny) + 1);
+            if(0 <= nx && nx < N && 0 <= ny && ny < N) {
+                if(map[nx][ny] > map[x][y]) {
+                    dp[x][y] = Math.max(dp[x][y], dfs(nx, ny) + 1);
+                }
             }
         }
 
-
         return dp[x][y];
-    }
-
-    public static boolean check(int x, int y) {
-        return 0<= x && x < N && 0 <= y && y < N;
     }
 }
