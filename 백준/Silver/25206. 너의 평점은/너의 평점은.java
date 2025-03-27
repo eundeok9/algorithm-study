389b427a497e01b3a723d1b9e9ea6ef9ba806db7
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2025-03-27 15:49:10 +0900

    [Silver V] Title: 너의 평점은, Time: 112 ms, Memory: 14636 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.java
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.java
@@ -5,47 +5,23 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
+        Map<String, Double> gradeMap = new HashMap<>();
+        gradeMap.put("A+", 4.5); gradeMap.put("A0", 4.0);
+        gradeMap.put("B+", 3.5); gradeMap.put("B0", 3.0);
+        gradeMap.put("C+", 2.5); gradeMap.put("C0", 2.0);
+        gradeMap.put("D+", 1.5); gradeMap.put("D0", 1.0);
+        gradeMap.put("F", 0.0);
+
         double totalCredit = 0; // 총 이수 학점
         double totalGrade = 0; // 성적 총합
         for(int i=0; i<20; i++) {
             st = new StringTokenizer(br.readLine());
-            String name = st.nextToken();
+            st.nextToken();
             double credit = Double.parseDouble(st.nextToken());
             String grade = st.nextToken();
 
-            switch(grade) {
-                case "A+":
-                    totalGrade += credit * 4.5;
-                    break;
-                case "A0":
-                    totalGrade += credit * 4.0;
-                    break;
-                case "B+":
-                    totalGrade += credit * 3.5;
-                    break;
-                case "B0":
-                    totalGrade += credit * 3.0;
-                    break;
-                case "C+":
-                    totalGrade += credit * 2.5;
-                    break;
-                case "C0":
-                    totalGrade += credit * 2.0;
-                    break;
-                case "D+":
-                    totalGrade += credit * 1.5;
-                    break;
-                case "D0":
-                    totalGrade += credit;
-                    break;
-                case "F":
-                    totalGrade += 0;
-                    break;
-                case "P":
-                    break;
-            }
-
             if(grade.equals("P")) continue;
+            totalGrade += credit * gradeMap.get(grade);
             totalCredit += credit;
         }
 
