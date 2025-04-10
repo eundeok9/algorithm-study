2b27715db67b71370925b0de1d7759a4457ea90a
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2025-04-10 17:17:22 +0900

    [level 3] Title: 네트워크, Time: 0.49 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,31 +1,37 @@
-import java.util.*;
-
 class Solution {
-    static boolean[] visited;
-    static ArrayList<Integer> graph;
-    
+    static int[] parent;
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        graph = new ArrayList<>();
-        visited = new boolean[n];
+        
+        parent = new int[n];
+        for(int i=0; i<n; i++) parent[i] = i;
         
         for(int i=0; i<n; i++) {
-            if(!visited[i]) {
-                dfs(computers, i);
-                answer++;
+            for(int j=0; j<n; j++) {
+                if(i!=j && computers[i][j] == 1) {
+                    union(i, j);
+                }
             }
+        }
+        
+        for(int i=0; i<n; i++) {
+            if(parent[i] == i) answer++;
         }
         
         return answer;
     }
     
-    public static void dfs(int[][] computers, int index) {
-        visited[index] = true;
+    public static void union(int a, int b) {
+        int pa = find(a);
+        int pb = find(b);
         
-        for(int i=0; i<computers.length; i++) {
-            if(i!=index && !visited[i] && computers[index][i] == 1) {
-                dfs(computers, i);
-            }
+        if(pa != pb) {
+            parent[pb] = pa;
         }
+    }
+    
+    public static int find(int x) {
+        if(parent[x] != x) parent[x] = find(parent[x]);
+        return parent[x];
     }
 }
