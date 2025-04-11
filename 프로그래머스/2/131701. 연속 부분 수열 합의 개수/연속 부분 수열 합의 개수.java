52890b5e032b37146b851615ae2d69a233fe79fc
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2025-04-11 17:29:13 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 189.11 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -2,28 +2,18 @@ import java.util.*;
 class Solution {
     public int solution(int[] elements) {
         HashSet<Integer> set = new HashSet<>();
+        int n = elements.length;    
         
-        int n = elements.length;
-        int sum = 0;
+        // 누적합 구하기
+        int[] arr = new int[2*n+1];
+        for(int i=0; i<2*n; i++) {
+            arr[i+1] = arr[i] + elements[i%n];
+        }
+        
+        // 1~n개 선택 시 나올 수 있는 합
         for(int i=0; i<n; i++) {
-            set.add(elements[i]); // 1개 선택 시 나올 수 있는 합
-            sum += elements[i];
-        }
-        set.add(sum); // n개 선택 시 나올 수 있는 합
-        
-        int[] arr = new int[n*2 - 2]; // 누적합 저장
-        arr[0] = elements[0];
-        for(int i=1; i<n; i++) {
-            arr[i] = arr[i-1] + elements[i];   
-        }
-        for(int i=0; i<n-2; i++) {
-            arr[n+i] = arr[n+i-1] + elements[i];
-        }
-        
-        for(int cnt=2; cnt<n; cnt++) {// 2~n-1개 선택 시 나올 수 있는 합
-            set.add(arr[cnt-1]);
-            for(int i=cnt; i<cnt+n-1; i++) {
-                set.add(arr[i]-arr[i-cnt]);
+            for(int L=1; L<=n; L++) {
+                set.add(arr[i+L] - arr[i]);
             }
         }
         
