89586e0d23008c3c00339f9851d3f6e617719fb3
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2025-04-23 16:34:02 +0900

    [Gold IV] Title: 최단경로, Time: 776 ms, Memory: 109888 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,93 +1,52 @@
-import java.util.*;
 import java.io.*;
-
+import java.util.*;
 public class Main {
+    static class Node implements Comparable<Node> {
+        int v, w;
+        Node(int v, int w) {this.v = v; this.w = w;}
 
-	static class Node implements Comparable<Node>{
-		int vertex, weight;
-		
-		public Node(int vertex, int weight) {
-			this.vertex= vertex;
-			this.weight = weight;
-		}
+        public int compareTo( Node o) {
+            return w - o.w;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		@Override
-		public int compareTo(Node o) {
-			return Integer.compare(this.weight, o.weight);
-		}
-	}
-	
-	static int V, E;
-	static ArrayList<Node>[] list;
-	static boolean[] visited;
-	static int[] distance;
-	static final int INF = Integer.MAX_VALUE;
-	static PriorityQueue<Node> pq;
-	static StringBuilder sb = new StringBuilder();
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null;
-		
-		
-		st = new StringTokenizer(br.readLine());
-		V = Integer.parseInt(st.nextToken()); // 정점 개수
-		E = Integer.parseInt(st.nextToken()); // 간선 개수
-		
-		int start = Integer.parseInt(br.readLine()); // 시작 정점 번호
-		
-		list = new ArrayList[V+1];
-		
-		for(int i=0; i<=V; i++) {
-			list[i] = new ArrayList<>();
-		}
-		
-		for(int i=0; i<E; i++) {
-			st = new StringTokenizer(br.readLine());
-			int from = Integer.parseInt(st.nextToken());
-			int to = Integer.parseInt(st.nextToken());
-			int weight = Integer.parseInt(st.nextToken());
-			list[from].add(new Node(to, weight));
-		}
-		
-		
-		dijkstra(start);
-		
-		System.out.println(sb);
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
 
-	}
-	
-	static void dijkstra(int start) {
-		// -- 다익스트라 start by PriorityQueue (하나씩 방문해서 최소값을 찾는 건 O(N^2) -> 시간 초과)
-		visited = new boolean[V+1];
-		distance = new int[V+1];
-		
-		Arrays.fill(distance, INF);
+        int start = Integer.parseInt(br.readLine());
 
-		distance[start] = 0;
-		
-		PriorityQueue<Node> pq = new PriorityQueue<>();
-		pq.offer(new Node(start, 0));
-		
-		while(!pq.isEmpty()) {
-			int cur = pq.poll().vertex;
-			
-			if(visited[cur]) continue;
-			visited[cur] = true;
-			
-			for(Node next : list[cur]) {
-				if(distance[next.vertex] > distance[cur] + next.weight) {
-					distance[next.vertex]  = distance[cur] + next.weight;
-					pq.offer(new Node(next.vertex, distance[next.vertex]));
-				}
-			}
-		}
-		
-		for(int i =1; i<distance.length; i++) {
-			if(distance[i]==INF) sb.append("INF\n");
-			else sb.append(distance[i] + "\n");
-		}
-		// -- end
-	}
+        ArrayList<Node>[] graph = new ArrayList[V+1];
+        for(int i=1; i<=V; i++) graph[i] = new ArrayList<>();
+        for(int i=0; i<E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            graph[u].add(new Node(v, w));
+        }
 
+        final int INF = 1_000_000_000;
+        int[] dist = new int[V+1];
+        Arrays.fill(dist, INF);
+        dist[start] = 0;
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(start, 0));
+        while(!pq.isEmpty()) {
+            Node cur = pq.poll();
+            if(cur.w > dist[cur.v]) continue;
+            for(Node nxt: graph[cur.v]) {
+                if(dist[nxt.v] > cur.w + nxt.w) {
+                    dist[nxt.v] = cur.w + nxt.w;
+                    pq.add(new Node(nxt.v, dist[nxt.v]));
+                }
+            }
+        }
+
+        for(int i=1; i<=V; i++) {
+            System.out.println(dist[i] != INF ? dist[i] : "INF");
+        }
+    }
 }
