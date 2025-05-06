f128c46a6e58cbb67572e60231f395ab5d4e30a2
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2025-05-06 22:54:01 +0900

    [Silver V] Title: 영화감독 숌, Time: 320 ms, Memory: 158464 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,18 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int count = 1;
+        int number = 666;
+        while(count != n) {
+            number++;
+            if(String.valueOf(number).contains("666")) {
+                count++;
+            }
+        }
+        System.out.println(number);
+    }
+}
