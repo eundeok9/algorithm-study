93ea22b927e8e50f66db86e178f6786f35f80293
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2025-06-24 16:33:28 +0900

    [Gold III] Title: 양팔저울, Time: 72 ms, Memory: 12288 KB -BaekjoonHub

---
+++ 백준/Gold/2629. 양팔저울/양팔저울.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static boolean[][] dp;
+    static int[] weights;
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+        weights = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            weights[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dp = new boolean[N + 1][15001];
+        dfs(0, 0);
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            int q = Integer.parseInt(st.nextToken());
+            if (q > 15000) sb.append("N ");
+            else sb.append(dp[N][q] ? "Y " : "N ");
+        }
+
+        System.out.println(sb);
+    }
+
+    static void dfs(int idx, int weight) {
+        if (weight > 15000) return;
+        if (idx > N) return;
+        if (dp[idx][weight]) return;
+
+        dp[idx][weight] = true;
+        if (idx == N) return;
+
+        dfs(idx + 1, weight + weights[idx]); // 왼쪽에 올리기
+        dfs(idx + 1, Math.abs(weight - weights[idx])); // 오른쪽에 올리기
+        dfs(idx + 1, weight); // 사용 안 함
+    }
+}
