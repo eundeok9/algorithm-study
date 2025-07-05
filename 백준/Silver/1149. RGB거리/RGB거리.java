b2f5455c3d888e417b211ff6863ae7c46d6c489b
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2025-07-05 15:22:46 +0900

    [Silver I] Title: RGB거리, Time: 72 ms, Memory: 12072 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -7,23 +7,18 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
         int[][] dp = new int[N][3];
-        int[][] map = new int[N][3];
 
         for(int i=0; i<N; i++) {
             st = new StringTokenizer(br.readLine());
             for(int j=0; j<3; j++) {
-                int cost = Integer.parseInt(st.nextToken());
-                map[i][j] = cost;
-                if(i==0) {
-                    dp[i][j] = cost;
-                }
+                dp[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
         for(int i=1; i<N; i++) {
-            dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2]) + map[i][0];
-            dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2]) + map[i][1];
-            dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1]) + map[i][2];
+            dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2]) + dp[i][0];
+            dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2]) + dp[i][1];
+            dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1]) + dp[i][2];
         }
 
         int answer = Integer.MAX_VALUE;
