14ab60638bea9272ecfee47e7ea3a8b8780938e0
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2025-07-21 15:19:59 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 556 ms, Memory: 74488 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -7,38 +7,39 @@ public class Main {
         StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
-
         List<List<Integer>> graph = new ArrayList<>();
-        for(int i=0; i<N; i++) {
+        for(int i=0; i<=N; i++) {
             graph.add(new ArrayList<>());
         }
+
         for(int i=0; i<N-1; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken()) - 1;
-            int b = Integer.parseInt(st.nextToken()) - 1;
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
             graph.get(a).add(b);
             graph.get(b).add(a);
         }
 
-        int[] parent = new int[N];
-        boolean[] visited = new boolean[N];
+        int[] parent = new int[N+1];
+        boolean[] visited = new boolean[N+1];
         Queue<Integer> queue = new LinkedList<>();
-        queue.add(0);
-        visited[0] = true;
+
+        queue.offer(1);
+        visited[1] = true;
 
         while(!queue.isEmpty()) {
             int cur = queue.poll();
 
             for(int nxt: graph.get(cur)) {
                 if(!visited[nxt]) {
-                    parent[nxt] = cur+1;
                     visited[nxt] = true;
-                    queue.add(nxt);
+                    parent[nxt] = cur; // 부모 정보 저장
+                    queue.offer(nxt);
                 }
             }
         }
 
-        for(int i=1; i<N; i++) {
+        for(int i=2; i<=N; i++) {
             sb.append(parent[i]).append("\n");
         }
         System.out.println(sb);
