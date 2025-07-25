e173f28f18ab1ff14ad7e5fb4472dd1bf9ee2054
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2025-07-25 14:24:32 +0900

    [Silver IV] Title: 점프왕 쩰리 (Small), Time: 64 ms, Memory: 11464 KB -BaekjoonHub

--- 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）.java
+++ 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）.java
@@ -3,10 +3,10 @@ import java.util.*;
 public class Main {
     static int N;
     static int[][] map;
-    static int[] dx = {0, 1};
-    static int[] dy = {1, 0};
     static boolean[][] visited;
-
+    static int[] dx = {1, 0};
+    static int[] dy = {0, 1};
+    static boolean isPossible = false;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -22,18 +22,18 @@ public class Main {
         }
 
         dfs(0, 0);
-        System.out.println("Hing");
+        System.out.println(isPossible ? "HaruHaru" : "Hing");
     }
 
-    static void dfs(int startX, int startY) {
-        if(startX == N-1 && startY == N-1) {
-            System.out.println("HaruHaru");
-            System.exit(0);
+    static void dfs(int i, int j) {
+        if(i == N-1 && j == N-1) {
+            isPossible = true;
+            return;
         }
 
         for(int d=0; d<2; d++) {
-            int nx = startX + dx[d] * map[startX][startY];
-            int ny = startY + dy[d] * map[startX][startY];
+            int nx = i + dx[d] * map[i][j];
+            int ny = j + dy[d] * map[i][j];
             if(nx >= N || ny >= N || visited[nx][ny]) continue;
 
             visited[nx][ny] = true;
