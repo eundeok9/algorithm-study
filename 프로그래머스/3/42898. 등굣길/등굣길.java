18777b19e9bbe3ecb8d07dc1faeb368189eb3bfd
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2025-07-29 18:36:11 +0900

    [level 3] Title: 등굣길, Time: 1.05 ms, Memory: 54.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public int solution(int m, int n, int[][] puddles) {
+        final int MOD = 1000000007;
+        
+        int[][] map = new int[n+1][m+1];
+        for(int i=0; i<puddles.length; i++) {
+            map[puddles[i][1]][puddles[i][0]] = -1;
+        }
+        
+        map[1][1] = 1;
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=m; j++) {
+                if(map[i][j] == -1) continue;
+                if(map[i-1][j] > 0) map[i][j] += map[i-1][j] % MOD;
+                if(map[i][j-1] > 0) map[i][j] += map[i][j-1] % MOD;
+            }
+        }
+        
+        return map[n][m] % MOD;
+    }
+}
