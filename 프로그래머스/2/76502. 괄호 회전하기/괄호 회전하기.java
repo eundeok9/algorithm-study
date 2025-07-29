d977ebd6583c7fbf7c62d9883df545bc72fcc3de
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2025-07-29 18:15:56 +0900

    [level 2] Title: 괄호 회전하기, Time: 41.12 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -0,0 +1,54 @@
+import java.util.*;
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        char[] carr = new char[s.length() * 2];
+        
+        for(int i=0; i<s.length(); i++) {
+            carr[i] = s.charAt(i);
+            carr[i+s.length()] = s.charAt(i);
+        }
+        
+       
+        for(int i=0; i<s.length(); i++) {
+            Stack<Character> stack = new Stack<>();
+            
+            boolean flag = true;
+            for(int j=i; j<i+s.length(); j++) {
+                char ch = carr[j];
+                if(ch == '[' || ch == '{' || ch == '(') {
+                    stack.push(ch);
+                } else if(ch == ']') {
+                    if(stack.isEmpty()) {
+                        flag = false;
+                        break;
+                    }
+                    if(!stack.isEmpty() && stack.peek() == '[') {
+                        stack.pop();
+                    }
+                } else if(ch == '}') {
+                    if(stack.isEmpty()) {
+                        flag = false;
+                        break;
+                    }
+                    if(!stack.isEmpty() && stack.peek() == '{') {
+                        stack.pop();
+                    }
+                } else if(ch == ')') {
+                    if(stack.isEmpty()) {
+                        flag = false;
+                        break;
+                    }
+                    if(!stack.isEmpty() && stack.peek() == '(') {
+                        stack.pop();
+                    }
+                }
+            }
+            if(stack.isEmpty() && flag) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
