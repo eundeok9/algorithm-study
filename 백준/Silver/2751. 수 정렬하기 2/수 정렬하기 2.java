fdadcc5b389db0a57943e9d27f5065060a7d8124
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2025-08-12 15:40:28 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1188 ms, Memory: 159944 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=0; i<N; i++) {
+            list.add(Integer.parseInt(br.readLine()));
+        }
+
+        Collections.sort(list);
+
+        StringBuilder sb = new StringBuilder();
+        for(int n: list) {
+            sb.append(n).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
