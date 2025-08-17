f151f8a6ee169ed7dd1df72280b04780af5a846f
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2025-08-17 14:29:57 +0900

    [Gold IV] Title: 줄세우기, Time: 64 ms, Memory: 11484 KB -BaekjoonHub

---
+++ 백준/Gold/2631. 줄세우기/줄세우기.java
@@ -0,0 +1,27 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        for(int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[] dp = new int[N];
+        dp[0] = 1;
+
+        int longest = 0; // 오름차순 개수 최대
+        for(int i=1; i<N; i++) {
+            dp[i] = 1;
+            for(int j=0; j<i; j++) {
+                if(arr[i]>arr[j]) dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+            longest = Math.max(longest, dp[i]);
+        }
+
+        System.out.println(N - longest);
+    }
+}
