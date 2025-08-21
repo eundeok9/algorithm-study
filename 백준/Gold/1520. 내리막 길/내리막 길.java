b1d5ebc5d03eecbf5ac5c445478a718cef406037
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2025-08-21 14:53:48 +0900

    [Gold III] Title: 내리막 길, Time: 308 ms, Memory: 39812 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -1,13 +1,10 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-    static int N, M; // 세로, 가로
-    static int[][] map;
-    static int[][] dp;
-    static int answer = 0;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-
+    static int N, M;
+    static int[][] map, dp;
+    static int[] dx = {0, 0, 1, -1};
+    static int[] dy = {1, -1, 0, 0};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -17,21 +14,19 @@ public class Main {
 
         map = new int[N][M];
         dp = new int[N][M];
+
         for(int i=0; i<N; i++) {
+            Arrays.fill(dp[i], -1);
             st = new StringTokenizer(br.readLine());
             for(int j=0; j<M; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        for(int i=0; i<N; i++) {
-            Arrays.fill(dp[i], -1);
-        }
-
         System.out.println(dfs(0, 0));
     }
 
-    public static int dfs(int x, int y) {
+    static int dfs(int x, int y) {
         if(x == N-1 && y == M-1) {
             return 1;
         }
@@ -41,22 +36,18 @@ public class Main {
         }
 
         dp[x][y] = 0;
+        for(int i=0; i<4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
 
-        for(int d=0; d<4; d++) {
-            int nx = x + dx[d];
-            int ny = y + dy[d];
+            if(nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
 
-            if(!isRange(nx, ny)) continue;
 
-            if(map[nx][ny] < map[x][y]) {
+            if(map[x][y] > map[nx][ny]) {
                 dp[x][y] += dfs(nx, ny);
             }
         }
 
         return dp[x][y];
-    }
-
-    public static boolean isRange(int x, int y) {
-        return 0 <= x && x < N && 0 <= y && y < M;
     }
 }
