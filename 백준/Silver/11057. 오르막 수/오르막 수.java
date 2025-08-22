902b0f920757ce05ad2ac9122d7f82ecb4162fae
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2025-08-22 14:17:38 +0900

    [Silver I] Title: 오르막 수, Time: 60 ms, Memory: 11536 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.java
+++ 백준/Silver/11057. 오르막 수/오르막 수.java
@@ -1,27 +1,27 @@
 import java.io.*;
 public class Main {
+    static final int MOD = 10007;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
+        int[][] dp = new int[N+1][10];
 
-        int[][] dp = new int[N + 1][10];
-        for (int i = 0; i < 10; i++) {
-            dp[1][i] = 1;
+        for(int j=0; j<10; j++) {
+            dp[1][j] = 1;
         }
 
-        for (int i = 2; i <= N; i++) {
+        for(int i=2; i<=N; i++) {
             dp[i][0] = dp[i-1][0];
-            for (int j = 1; j < 10; j++) {
-                dp[i][j] = (dp[i][j-1] + dp[i-1][j]) % 10007;
+            for(int j=1; j<10; j++) {
+                dp[i][j] = (dp[i][j-1] + dp[i-1][j]) % MOD;
             }
         }
 
-        int sum = 0;
-        for (int i = 0; i < 10; i++) {
-            sum = (sum + dp[N][i]) % 10007;
+        int answer = 0;
+        for(int j=0; j<10; j++) {
+            answer = (answer + dp[N][j]) % MOD;
         }
-
-        System.out.println(sum);
+        System.out.println(answer);
     }
 }
