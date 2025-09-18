4c9708dd857115ff525d6adbda0ec95bedfaf99f
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2025-09-18 23:26:01 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -1,9 +1,9 @@
-select case
-    when (d.skill_code & (select sum(code) from skillcodes where category like 'Front%')) and (d.skill_code & (select code from skillcodes where name = 'Python')) then 'A'
-    when (d.skill_code & (select code from skillcodes where name = 'C#')) then 'B'
-    when (d.skill_code & (select sum(code) from skillcodes where category like 'Front%')) then 'C'
-    else NULL
-end as grade, id, email
+SELECT CASE
+    WHEN (d.skill_code & (select sum(code) from skillcodes where category like "Front%")) and (d.skill_code & (select code from skillcodes where name = 'Python')) then 'A'
+    WHEN (d.skill_code & (select code from skillcodes where name = 'C#')) then 'B'
+    WHEN (d.skill_code & (select sum(code) from skillcodes where category like 'Front%')) then 'C'
+    ELSE NULL
+END as grade, id, email
 from developers as d
 group by grade, id, email
 having grade is not null
