c0789d8f96df160ad311d673e88d66325a0803c5
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2025-10-07 20:47:30 +0900

    [Gold V] Title: 동전 2, Time: 120 ms, Memory: 14520 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -6,25 +6,27 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken()); // 가치
+        int K = Integer.parseInt(st.nextToken());
 
-        int[] coin = new int[N+1];
-        for(int i=1; i<=N; i++) {
-            coin[i] = Integer.parseInt(br.readLine());
+        int[] coins = new int[N];
+        for(int i=0; i<N; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
         }
 
-        int[] dp = new int[K+1]; // dp[i]: i원을 만드는 동전의 개수
+        int[] dp = new int[K+1];
+
         Arrays.fill(dp, Integer.MAX_VALUE);
+
         dp[0] = 0;
 
-        for(int i=1; i<=N; i++) {
-            for(int j=coin[i]; j<=K; j++) {
-                if(dp[j - coin[i]] != Integer.MAX_VALUE) {
-                    dp[j] = Math.min(dp[j], dp[j - coin[i]] + 1);
+        for(int i=0; i<=K; i++) {
+            for(int coin: coins) {
+                if(i - coin >= 0 && dp[i-coin] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], dp[i-coin] + 1);
                 }
             }
         }
 
-        System.out.println((dp[K] == Integer.MAX_VALUE) ? -1 : dp[K]);
+        System.out.println(dp[K] == Integer.MAX_VALUE ? -1 : dp[K]);
     }
 }
