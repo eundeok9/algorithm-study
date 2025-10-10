454288e8f83a5f7803c66415ec73b1b66829efbc
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2025-10-10 17:09:47 +0900

    [Silver V] Title: 장신구 명장 임스, Time: 72 ms, Memory: 11952 KB -BaekjoonHub

---
+++ 백준/Silver/25496. 장신구 명장 임스/장신구 명장 임스.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int P = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int idx = 0;
+        int cnt = 0;
+        while(P < 200 && idx < N) {
+            P += arr[idx++];
+            cnt++;
+        }
+
+        System.out.println(cnt);
+    }
+}
