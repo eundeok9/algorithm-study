614cb94d9b67bde47c10a416e761167e7ce4ebd8
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2025-11-01 20:22:49 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,5 +1,7 @@
-SELECT i.ANIMAL_ID, i.NAME
-FROM ANIMAL_INS as i
-JOIN ANIMAL_OUTS as o ON i.ANIMAL_ID = o.ANIMAL_ID
-WHERE i.DATETIME > o.DATETIME
-ORDER BY i.DATETIME
+-- 코드를 입력하세요
+SELECT i.animal_id, i.name
+from animal_ins as i
+join animal_outs as o
+on i.animal_id=o.animal_id
+where o.datetime < i.datetime
+order by i.datetime;
