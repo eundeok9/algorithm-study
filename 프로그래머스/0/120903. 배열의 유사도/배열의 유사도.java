57d134cbd19f64fd60ba808bb9f5f6c80c140dd1
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2025-11-28 15:46:18 +0900

    [level 0] Title: 배열의 유사도, Time: 0.09 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
@@ -3,13 +3,10 @@ class Solution {
     public int solution(String[] s1, String[] s2) {
         int answer = 0;
         
-        HashSet<String> set = new HashSet<>();
-        for(String s: s1) {
-            set.add(s);    
-        }
+        List<String> list = Arrays.asList(s1);
         
         for(String s: s2) {
-            if(set.contains(s)) {
+            if(list.contains(s)) {
                 answer++;
             }
         }
