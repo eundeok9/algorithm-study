2bf05f566f50b39cfd0d507232df4884f7544e1c
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2025-12-12 17:45:03 +0900

    [level 3] Title: 이중우선순위큐, Time: 125.33 ms, Memory: 122 MB -BaekjoonHub

---
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] operations) {
+        int[] answer = new int[2];
+        
+        PriorityQueue<Integer> maxQueue = new PriorityQueue<>(Comparator.reverseOrder()); // 내림차순 정렬
+        PriorityQueue<Integer> minQueue = new PriorityQueue<>(); // 오름차순 정렬
+        
+        for(String operation: operations) {
+            String[] sp = operation.split(" ");
+            
+            int num = Integer.parseInt(sp[1]);
+            
+            if(sp[0].equals("I")) {
+                maxQueue.add(num);
+                minQueue.add(num);
+            } else if(sp[0].equals("D") && !maxQueue.isEmpty()) {
+                if(num == 1) {
+                    int max = maxQueue.poll();
+                    minQueue.remove(max);
+                } else {
+                    int min = minQueue.poll();
+                    maxQueue.remove(min);
+                }
+            }
+        }
+        
+        if(maxQueue.isEmpty()) return new int[] {0, 0};
+        
+        return new int[] {maxQueue.poll(), minQueue.poll()};
+    }
+}
