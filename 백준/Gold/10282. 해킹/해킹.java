f2b822e983767047aea88d0257b7c835b68e404a
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2026-01-01 23:40:52 +0900

    [Gold IV] Title: 해킹, Time: 776 ms, Memory: 149628 KB -BaekjoonHub

--- 백준/Gold/10282. 해킹/해킹.java
+++ 백준/Gold/10282. 해킹/해킹.java
@@ -1,94 +1,83 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
-    static class Computer implements Comparable<Computer> {
-        int d;
-        int t;
+    static int n, d, c;
+    static List<List<Dependency>> graph;
 
-        Computer(int d, int t) {
-            this.d = d;
-            this.t = t;
-        }
-
-        public int compareTo(Computer o) {
-            return this.t - o.t;
+    static class Dependency {
+        int computer, time;
+        Dependency(int computer, int time) {
+            this.computer = computer;
+            this.time = time;
         }
     }
 
-    static int INF = Integer.MAX_VALUE;
+    static class Node implements Comparable<Node> {
+        int v, dist;
+        Node(int v, int dist) { this.v = v; this.dist = dist; }
+        public int compareTo(Node o) { return Integer.compare(this.dist, o.dist); }
+    }
 
-    static List<List<Computer>> graph;
-    static int[] dist;
-    static boolean[] visited;
+    static int count;
+    static int time;
+    static final int INF = 1_000_000_000;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
         int T = Integer.parseInt(br.readLine());
+
         StringBuilder sb = new StringBuilder();
-
-        while(T-->0) {
-            st = new StringTokenizer(br.readLine());
-
-            int N = Integer.parseInt(st.nextToken());
-            int D = Integer.parseInt(st.nextToken());
-            int C = Integer.parseInt(st.nextToken());
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken());
+            d = Integer.parseInt(st.nextToken());
+            c = Integer.parseInt(st.nextToken());
 
             graph = new ArrayList<>();
-            dist = new int[N+1];
-            visited = new boolean[N+1];
+            for (int i = 0; i <= n; i++) graph.add(new ArrayList<>());
 
-            for(int i=0; i<=N; i++) {
-                graph.add(new ArrayList<>());
-                dist[i] = INF;
-            }
-
-            for(int i=0; i<D; i++) {
+            for (int i = 0; i < d; i++) {
                 st = new StringTokenizer(br.readLine());
-
                 int a = Integer.parseInt(st.nextToken());
                 int b = Integer.parseInt(st.nextToken());
                 int s = Integer.parseInt(st.nextToken());
-
-                graph.get(b).add(new Computer(a, s));
+                graph.get(b).add(new Dependency(a, s)); // b -> a (s초)
             }
 
-            dijkstra(C);
-
-            int cnt = 0;
-            int time = 0;
-
-            for(int i=1; i<=N; i++) {
-                if(dist[i] != INF) {
-                    cnt++;
-                    time = Math.max(time, dist[i]);
-                }
-            }
-            
-            sb.append(cnt).append(" ").append(time).append("\n");
+            dijkstra();
+            sb.append(count).append(" ").append(time).append("\n");
         }
-
-        System.out.println(sb);
+        System.out.print(sb);
     }
 
-    static void dijkstra(int start) {
-        PriorityQueue<Computer> pq = new PriorityQueue<>();
+    static void dijkstra() {
+        int[] dist = new int[n + 1];
+        Arrays.fill(dist, INF);
+        dist[c] = 0;
 
-        dist[start] = 0;
-        pq.offer(new Computer(start, 0));
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(c, 0));
 
-        while(!pq.isEmpty()) {
-            int cur = pq.poll().d;
+        while (!pq.isEmpty()) {
+            Node cur = pq.poll();
+            if (cur.dist != dist[cur.v]) continue; // 구버전 스킵
 
-            if(!visited[cur]) {
-                visited[cur] = true;
-
-                for(Computer nxt: graph.get(cur)) {
-                    if(dist[nxt.d] > dist[cur] + nxt.t) {
-                        dist[nxt.d] = dist[cur] + nxt.t;
-                        pq.offer(new Computer(nxt.d, dist[nxt.d]));
-                    }
+            for (Dependency nxt : graph.get(cur.v)) {
+                int nd = cur.dist + nxt.time;
+                if (nd < dist[nxt.computer]) {
+                    dist[nxt.computer] = nd;
+                    pq.offer(new Node(nxt.computer, nd));
                 }
+            }
+        }
+
+        count = 0;
+        time = 0;
+        for (int i = 1; i <= n; i++) {
+            if (dist[i] != INF) {
+                count++;
+                time = Math.max(time, dist[i]);
             }
         }
     }
