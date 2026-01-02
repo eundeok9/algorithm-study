d206dd84bf46bd284876ca687675aa072e67d53b
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2026-01-02 16:36:26 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 572 ms, Memory: 66084 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,47 +1,48 @@
 import java.io.*;
 import java.util.*;
 public class Main {
+    static List<List<Integer>> graph;
+    static boolean[] visited;
+    static int[] parent;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
-
+        
         int N = Integer.parseInt(br.readLine());
-        List<List<Integer>> graph = new ArrayList<>();
+        
+        graph = new ArrayList<>();
         for(int i=0; i<=N; i++) {
             graph.add(new ArrayList<>());
         }
-
+        
         for(int i=0; i<N-1; i++) {
-            st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
+            
             graph.get(a).add(b);
             graph.get(b).add(a);
         }
-
-        int[] parent = new int[N+1];
-        boolean[] visited = new boolean[N+1];
-        Queue<Integer> queue = new LinkedList<>();
-
-        queue.offer(1);
+        
+        parent = new int[N+1];
+        visited = new boolean[N+1];
         visited[1] = true;
-
-        while(!queue.isEmpty()) {
-            int cur = queue.poll();
-
-            for(int nxt: graph.get(cur)) {
-                if(!visited[nxt]) {
-                    visited[nxt] = true;
-                    parent[nxt] = cur; // 부모 정보 저장
-                    queue.offer(nxt);
-                }
-            }
-        }
-
+        dfs(1);
+        
+        StringBuilder sb = new StringBuilder();
         for(int i=2; i<=N; i++) {
             sb.append(parent[i]).append("\n");
         }
         System.out.println(sb);
+    }
+    
+    static void dfs(int cur) {
+        for(int nxt: graph.get(cur)) {
+            if(!visited[nxt]) {
+                visited[nxt] = true;
+                parent[nxt] = cur;
+                dfs(nxt);
+            }
+        }
     }
 }
