0173d609839dfc34cc23dbc922b841ede6b5d692
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2026-01-02 17:35:56 +0900

    [level 1] Title: 가장 큰 물고기 10마리 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/298517. 가장 큰 물고기 10마리 구하기/가장 큰 물고기 10마리 구하기.sql
+++ 프로그래머스/1/298517. 가장 큰 물고기 10마리 구하기/가장 큰 물고기 10마리 구하기.sql
@@ -1,15 +1,6 @@
 -- 코드를 작성해주세요
-with ranked_fish as (
-    select id, length, row_number() over(order by length desc, id) as rn
-    from fish_info
-)
-
 select id, length
-from ranked_fish
-where rn <= 10;
+from fish_info
+order by length desc
+limit 10;
 
--- limit을 사용하는 방법
-# select id, length
-# from fish_info
-# order by length desc, id
-# limit 10;
