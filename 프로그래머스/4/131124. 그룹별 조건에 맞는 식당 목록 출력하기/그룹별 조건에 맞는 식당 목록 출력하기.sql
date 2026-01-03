5730c125dd262b90d7e494260d11e64d77b2339d
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2026-01-03 16:01:55 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,10 +1,9 @@
 -- 코드를 입력하세요
-SELECT a.member_name, b.review_text, date_format(b.review_date, '%Y-%m-%d') as review_date
-from member_profile as a, rest_review as b
-where a.member_id = b.member_id and a.member_id = (
-    select member_id from rest_review
-    group by member_id
-    order by count(member_id) desc
-    limit 1
-)
+with review_count as (select member_id, count(*) from rest_review group by member_id order by count(*) desc)
+SELECT member_name, review_text, date_format(review_date, '%Y-%m-%d') as review_date
+from MEMBER_PROFILE as a
+join REST_REVIEW as b on a.MEMBER_ID = b.MEMBER_ID
+where a.MEMBER_ID = (select member_id from review_count limit 1)
 order by review_date, review_text;
+
+-- 리뷰를 가장 많이 작성한 회원의 리뷰들
