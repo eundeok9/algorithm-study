499594cc670383bc52b819849fd23ae559fa0be1
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2026-01-04 13:33:04 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 468 ms, Memory: 36736 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
@@ -2,30 +2,28 @@ import java.io.*;
 import java.util.*;
 public class Main {
     static int N;
-    static int[][] map, dp;
+    static int[][] map;
+    static int[][] dp;
     static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0 ,0, -1, 1};
+    static int[] dy = {0, 0, -1, 1};
+    static int answer = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
         N = Integer.parseInt(br.readLine());
 
         map = new int[N][N];
         dp = new int[N][N];
         for(int i=0; i<N; i++) {
-            st = new StringTokenizer(br.readLine());
-            Arrays.fill(dp[i], -1);
+            StringTokenizer st = new StringTokenizer(br.readLine());
             for(int j=0; j<N; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        int answer = 0;
         for(int i=0; i<N; i++) {
             for(int j=0; j<N; j++) {
-//                System.out.println(i + " " + j + " " + dfs(i, j));
-                answer = Math.max(answer, dfs(i, j));
+                int count = dfs(i, j);
+                answer = Math.max(answer, count);
             }
         }
 
@@ -33,19 +31,20 @@ public class Main {
     }
 
     static int dfs(int x, int y) {
-        if(dp[x][y] != -1) {
+        if(dp[x][y] != 0) {
             return dp[x][y];
         }
 
         dp[x][y] = 1;
+
         for(int d=0; d<4; d++) {
             int nx = x + dx[d];
             int ny = y + dy[d];
 
             if(nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
-            if(map[x][y] < map[nx][ny]) {
-                dp[x][y] = Math.max(dp[x][y], dfs(nx, ny) + 1);
-            }
+            if(map[nx][ny] <= map[x][y]) continue;
+
+            dp[x][y] = Math.max(dp[x][y], dfs(nx, ny) + 1);
         }
 
         return dp[x][y];
