4731b4288b2c8b55339d2a59dc68a5a003598823
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2026-01-05 13:50:19 +0900

    [Gold IV] Title: 미로만들기, Time: 104 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.java
+++ 백준/Gold/2665. 미로만들기/미로만들기.java
@@ -1,60 +1,54 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-    static class Node implements Comparable<Node> {
-        int x, y, cost;
-        Node (int x, int y, int cost) {
-            this.x = x;
-            this.y = y;
-            this.cost = cost;
-        }
-
-        @Override
-        public int compareTo(Node o) {
-            return this.cost - o.cost;
-        }
-    }
+    static int N;
+    static int[][] map;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[][] map = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            String line = br.readLine();
-            for (int j = 0; j < N; j++) {
-                map[i][j] = line.charAt(j) - '0';
+
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
+
+        for(int i=0; i<N; i++) {
+            String s = br.readLine();
+            for(int j=0; j<N; j++) {
+                map[i][j] = s.charAt(j) - '0';
             }
         }
 
-        final int INF = N * N + 1;
-        int[][] dist = new int[N][N];
-        for(int [] row: dist) Arrays.fill(row, INF);
+        System.out.println(bfs());
+    }
 
-        dist[0][0] = 0;
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        pq.offer(new Node(0, 0, dist[0][0]));
+    static int bfs() {
+        ArrayDeque<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[] {0, 0, 0});
+        map[0][0] = -1;
 
         int[] dx = {-1, 1, 0, 0};
         int[] dy = {0, 0, -1, 1};
+        while(!queue.isEmpty()) {
+            int[] cur = queue.poll();
 
-        while(!pq.isEmpty()) {
-            Node cur = pq.poll();
-            if(cur.cost > dist[cur.x][cur.y]) continue;
-            if(cur.x == N-1 && cur.y == N-1) break;
+            if(cur[0] == N-1 && cur[1] == N-1) {
+                return cur[2];
+            }
 
             for(int d=0; d<4; d++) {
-                int nx = cur.x + dx[d];
-                int ny = cur.y + dy[d];
+                int nx = cur[0] + dx[d];
+                int ny = cur[1] + dy[d];
 
                 if(nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
 
-                int nextCost = cur.cost + (map[nx][ny] == 0 ? 1 : 0);
-                if(nextCost < dist[nx][ny]) {
-                    dist[nx][ny] = nextCost;
-                    pq.offer(new Node(nx, ny, nextCost));
+                if(map[nx][ny] == 1) {
+                    queue.addFirst(new int[] {nx, ny, cur[2]});
+                } else if(map[nx][ny] == 0) {
+                    queue.offer(new int[] {nx, ny, cur[2] + 1});
                 }
+
+                map[nx][ny] = -1;
             }
         }
 
-        System.out.println(dist[N-1][N-1]);
+        return -1;
     }
 }
