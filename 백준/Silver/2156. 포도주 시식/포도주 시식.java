7f6eb0b71b14590aa80b4d6ed12c6167a34d0dbe
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2026-01-05 14:21:53 +0900

    [Silver I] Title: 포도주 시식, Time: 128 ms, Memory: 15072 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -4,26 +4,23 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-
         int[] wine = new int[N+1];
-        int[] dp = new int[N+1];
-
         for(int i=1; i<=N; i++) {
             wine[i] = Integer.parseInt(br.readLine());
         }
 
-        if(N == 1) {
-            System.out.println(wine[1]);
-        } else if(N==2) {
-            System.out.println(wine[1]+wine[2]);
-        } else {
-            dp[1] = wine[1];
-            dp[2] = dp[1] + wine[2];
-            for (int i = 3; i <= N; i++) {
-                dp[i] = Math.max(dp[i - 1], Math.max(dp[i - 2] + wine[i], dp[i - 3] + wine[i] + wine[i - 1]));
+        int[] dp = new int[N+1];
+        dp[0] = 0;
+        for(int i=1; i<=N; i++) {
+            if(i==1) {
+                dp[i] = wine[i];
+            } else if(i==2) {
+                dp[i] = wine[i] + wine[i-1];
+            } else {
+                dp[i] = Math.max(dp[i-1], Math.max(dp[i-2] + wine[i], dp[i-3] + wine[i] + wine[i-1]));
             }
-
-            System.out.println(dp[N]);
         }
+
+        System.out.println(dp[N]);
     }
 }
