71ef0f1afbd73fe8b4346d6d1da1de0e228a2dcc
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2026-01-05 16:47:00 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -1,14 +1,14 @@
-WITH FIRST AS (
-    SELECT ID
-    FROM ECOLI_DATA
-    WHERE PARENT_ID IS NULL
-),
-SECOND AS (
-    SELECT ecoli_data.id, ecoli_data.parent_id
-    FROM ECOLI_DATA, first
-    WHERE FIRST.ID = ECOLI_DATA.PARENT_ID
-)
+-- 코드를 작성해주세요
+select a.id
+from ecoli_data as a, ecoli_data as b, ecoli_data as c
+where a.parent_id = b.id and b.parent_id = c.id and c.parent_id is null
+order by a.id;
 
-select ecoli_data.id
-from ecoli_data, second
-where ecoli_data.parent_id = second.id
+
+-- 1세대(parent_id = null): 1, 2
+-- 2세대(parent_id = 1 or 2): 3, 4, 5
+-- 3세대(parent_id = 3 or 4 or 5): 6, 7
+-- 4세대(parent_id = 6 or 7): 8
+
+-- parent_id의 parent_id가 null
+-- a, b, c
