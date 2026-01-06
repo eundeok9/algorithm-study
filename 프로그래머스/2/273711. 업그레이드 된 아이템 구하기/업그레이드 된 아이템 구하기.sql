f19ba81ef191b410b499961f98e5a851a6b1d6e6
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2026-01-06 21:39:00 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -1,16 +1,12 @@
 -- 코드를 작성해주세요
+
+-- rare인 아이템의 다음 업그레이드 아이템
+
+-- 0, 1, 3, 4
+-- 0 -> 1, 2
+-- 1 -> 3, 4
+
 select c.item_id, c.item_name, c.rarity
-from item_info as a
-join item_tree as b
-join item_info as c
-where a.rarity = 'RARE' and a.item_id = b.PARENT_ITEM_ID and b.item_id = c.item_id
-order by c.item_id desc;
-
--- 희귀도가 'rare' 인 아이템들의 다음 업그레이드 아이템?
-
--- rare인 아이템
--- item_id -> parent_item_id
--- item_a (0) -> item_b, item_c
--- item_b (1) -> item_d, item_e
--- item_d (2) -> 더이상 x
--- item_e (2) -> 더이상 x
+from item_info as a, item_tree as b, item_info as c
+where a.item_id = b.parent_item_id and a.rarity = 'rare' and b.item_id = c.item_id
+order by item_id desc;
