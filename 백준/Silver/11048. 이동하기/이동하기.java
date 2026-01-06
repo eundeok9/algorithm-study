6761b775f9ec84575c40749dec23d1767f02853f
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2026-01-06 15:44:35 +0900

    [Silver II] Title: 이동하기, Time: 472 ms, Memory: 77020 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.java
+++ 백준/Silver/11048. 이동하기/이동하기.java
@@ -18,7 +18,6 @@ public class Main {
             }
         }
 
-        dp[1][1] = map[1][1];
         for(int i=1; i<=N; i++) {
             for(int j=1; j<=M; j++) {
                 dp[i][j] = Math.max(dp[i-1][j], Math.max(dp[i][j-1], dp[i-1][j-1])) + map[i][j];
