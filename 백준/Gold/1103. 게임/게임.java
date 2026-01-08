b2603693b1d80fd31ddfb3fb70e38b49f3d2d577
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2026-01-08 13:10:27 +0900

    [Gold II] Title: 게임, Time: 124 ms, Memory: 15992 KB -BaekjoonHub

--- 백준/Gold/1103. 게임/게임.java
+++ 백준/Gold/1103. 게임/게임.java
@@ -1,14 +1,14 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-    static boolean isCycle = false;
     static int N, M;
-    static char[][] map;
+    static int[][] map;
     static int[][] dp;
     static boolean[][] visited;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
-    static int max = -1;
+    static boolean isCycle;
+    static int max = Integer.MIN_VALUE;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -16,13 +16,20 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        map = new char[N][M];
-        dp = new int[N][M];
-        visited = new boolean[N][M];
-        for(int i=0; i<N; i++) {
-            map[i] = br.readLine().toCharArray();
+        map = new int[N][M];
+        for(int i=0; i<N; i++){
+            String s = br.readLine();
+            for(int j=0; j<M; j++) {
+                if(s.charAt(j) == 'H') {
+                    map[i][j] = -1;
+                } else {
+                    map[i][j] = s.charAt(j) - '0';
+                }
+            }
         }
 
+        dp = new int[N][M];
+        visited = new boolean[N][M];
         visited[0][0] = true;
         dfs(0, 0, 1);
 
@@ -33,29 +40,32 @@ public class Main {
         }
     }
 
-    public static void dfs(int x, int y, int depth) {
-        if(depth > max) {
-            max = depth;
+    static void dfs(int x, int y, int cnt) {
+        if(cnt > max) {
+            max = cnt;
         }
 
-        dp[x][y] = depth;
+        dp[x][y] = cnt;
 
         for(int d=0; d<4; d++) {
-            int nx = x + (dx[d] * (map[x][y] - '0'));
-            int ny = y + (dy[d] * (map[x][y] - '0'));
+            int nx = x + dx[d] * map[x][y];
+            int ny = y + dy[d] * map[x][y];
 
-            if(nx < 0 || nx >= N || ny < 0 || ny >= M || map[nx][ny] == 'H') continue;
+            if(nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+            if(map[nx][ny] == -1) continue;
 
-            if(visited[nx][ny]){
+            if(visited[nx][ny]) {
                 isCycle = true;
                 return;
             }
 
-            if(dp[nx][ny] > depth) continue;
+            if(dp[nx][ny] <= cnt) {
+                visited[nx][ny] = true;
+                dfs(nx, ny, cnt+1);
+                visited[nx][ny] = false;
+            }
 
-            visited[nx][ny] = true;
-            dfs(nx, ny, depth + 1);
-            visited[nx][ny] = false;
         }
     }
+
 }
