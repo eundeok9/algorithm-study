be8d1992c4b679d9edc968aecb4f78ccabec00c1
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2026-01-14 16:56:28 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 304 ms, Memory: 27552 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
@@ -5,41 +5,34 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        int[][] map = new int[n+1][m+1];
-        int[][] prefixSum = new int[n+1][m+1];
+        int[][] map = new int[N][M];
+        int[][] dp = new int[N][M];
 
-        for(int i=1; i<=n; i++) {
+        for(int i=0; i<N; i++) {
             String s = br.readLine();
-            for(int j=1; j<=m; j++) {
-                map[i][j] = s.charAt(j-1) - '0';
-                prefixSum[i][j] = map[i][j] + prefixSum[i-1][j] + prefixSum[i][j-1] - prefixSum[i-1][j-1];
+            for(int j=0; j<M; j++) {
+                map[i][j] = s.charAt(j) - '0';
             }
         }
 
-        int maxSize = 0;
-
-        for(int size = Math.min(n, m); size > 0; size--) {
-            boolean found = false;
-
-            for(int i=size; i<=n; i++) {
-                for(int j=size; j<=m; j++) {
-                    // (i, j)를 우하단 꼭짓점으로 하는 size*size 부분합 계산
-                    int total = prefixSum[i][j] - prefixSum[i-size][j] - prefixSum[i][j-size] + prefixSum[i-size][j-size];
-
-                    if(total == size * size) { // 모든 값이 1일 경우
-                        maxSize = size;
-                        found = true;
-                        break;
+        int max = 0;
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<M; j++) {
+                if(map[i][j] == 1) {
+                    if(i==0 || j==0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = Math.min(dp[i-1][j], Math.min(dp[i][j-1], dp[i-1][j-1])) + 1;
                     }
                 }
-                if(found) break;
+
+                max = Math.max(dp[i][j], max);
             }
-            if(found) break;
         }
 
-        System.out.println(maxSize * maxSize);
+        System.out.println(max*max);
     }
 }
