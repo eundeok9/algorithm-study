347b906058da67097bbd7ef683b980c4841f1768
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2026-01-17 00:56:20 +0900

    [Gold IV] Title: 합이 0, Time: 432 ms, Memory: 19708 KB -BaekjoonHub

--- 백준/Gold/3151. 합이 0/합이 0.java
+++ 백준/Gold/3151. 합이 0/합이 0.java
@@ -3,7 +3,6 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int N = Integer.parseInt(br.readLine());
         int[] arr = new int[N];
 
@@ -14,42 +13,44 @@ public class Main {
 
         Arrays.sort(arr);
 
-        long cnt = 0;
-        for(int i=0; i<N-1; i++) {
-            if(arr[i] > 0) break;
+        long count = 0;
+        for(int i=0; i<N-2; i++) {
+            if(arr[i] > 0) break; // 초기값이 양수이면 그 다음부터는 다 양수이므로 합이 0이 될 수 없음
 
-            int left = i+1, right = N-1;
-
-            while(left<right) {
-                long sum = (long) arr[i] + arr[left] + arr[right];
+            int left = i + 1, right = N-1;
+            while(left < right) {
+                int sum = arr[left] + arr[right] + arr[i];
 
                 if(sum == 0) {
                     if(arr[left] == arr[right]) {
+                        // left ~ right까지 모두 같은 수라는 뜻
                         int n = right - left + 1;
-                        cnt += (long) n * (n-1) / 2;
-                        break; // left부터 right까지 모두 탐색했으니 break
+                        count += (long) n * (n-1) / 2;
+                        break;
                     } else {
-                        int leftCnt = 1; int rightCnt = 1;
+                        // left와 같은 수 / right와 같은 수의 개수 구하기
+                        int cntLeft = 1, cntRight = 1;
                         while(left + 1 < right && arr[left] == arr[left+1]) {
-                            leftCnt++;
+                            cntLeft++;
                             left++;
                         }
-                        while(right -1 > left && arr[right] == arr[right-1]) {
-                            rightCnt++;
+                        while(right - 1 > left && arr[right] == arr[right-1]) {
+                            cntRight++;
                             right--;
                         }
-
-                        cnt += (long) leftCnt * rightCnt;
+                        count += (long) cntLeft * cntRight;
                         left++;
                         right--;
                     }
-                } else if(sum < 0) {
+
+                }
+                else if(sum < 0) {
                     left++;
                 } else {
                     right--;
                 }
             }
         }
-        System.out.println(cnt);
+        System.out.println(count);
     }
 }
