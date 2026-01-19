9611efd79f8427772eda83ce686c79adc062600e
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2026-01-19 21:09:24 +0900

    [Gold IV] Title: 동전 1, Time: 116 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.java
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -1,20 +1,18 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-    static int N, K;
-    static int[] coin, dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-        coin = new int[N+1];
-        dp = new int[K+1];
+        int[] coin = new int[N];
+        int[] dp = new int[K+1];
         dp[0] = 1;
 
-        for(int i=1; i<=N; i++) {
+        for(int i=0; i<N; i++) {
             coin[i] = Integer.parseInt(br.readLine());
             for(int j=coin[i]; j<=K; j++) {
                 dp[j] += dp[j-coin[i]];
