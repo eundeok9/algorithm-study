5f45cb7f91890f9e4f94d56ac2fa6ffe652fc750
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2026-01-21 13:44:50 +0900

    [Silver II] Title: 연속합, Time: 204 ms, Memory: 24328 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -4,20 +4,20 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
+        int N = Integer.parseInt(br.readLine());
 
+        int[] arr = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0; i<n; i++) {
+        for(int i=0; i<N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int[] dp = new int[n];
+        int[] dp = new int[N];
         dp[0] = arr[0];
-        int max = dp[0];
-        for(int i=1; i<n; i++) {
-            dp[i] = Math.max(dp[i-1] + arr[i], arr[i]);
-            max = Math.max(dp[i], max);
+        int max = arr[0];
+        for(int i=1; i<N; i++) {
+            dp[i] = Math.max(arr[i], dp[i-1] + arr[i]);
+            max = Math.max(max, dp[i]);
         }
 
         System.out.println(max);
