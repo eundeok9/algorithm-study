4526b24611fb87aca80a79f01fe7b6ee79a17ef2
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2026-01-22 20:23:56 +0900

    [Silver I] Title: 스티커, Time: 648 ms, Memory: 109940 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -3,29 +3,28 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
         int T = Integer.parseInt(br.readLine());
         while(T-->0) {
             int N = Integer.parseInt(br.readLine());
-            int[][] sticker = new int[2][N];
-            int[][] dp = new int[2][N];
+
+            int[][] arr = new int[2][N];
             for(int i=0; i<2; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
                 for(int j=0; j<N; j++) {
-                    sticker[i][j] = Integer.parseInt(st.nextToken());
+                    arr[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
-            dp[0][0] = sticker[0][0];
-            dp[1][0] = sticker[1][0];
-            for(int col=1; col<N; col++) {
-                dp[0][col] = Math.max(dp[1][col-1] + sticker[0][col], dp[0][col-1]);
-                dp[1][col] = Math.max(dp[0][col-1] + sticker[1][col], dp[1][col-1]);
+
+            int[][] dp = new int[2][N];
+            dp[0][0] = arr[0][0];
+            dp[1][0] = arr[1][0];
+            for(int j=1; j<N; j++) {
+                dp[0][j] = Math.max(dp[1][j-1] + arr[0][j], dp[0][j-1]);
+                dp[1][j] = Math.max(dp[0][j-1] + arr[1][j], dp[1][j-1]);
             }
 
-            int answer = Math.max(dp[0][N-1], dp[1][N-1]);
-            sb.append(answer).append("\n");
+            System.out.println(Math.max(dp[0][N-1], dp[1][N-1]));
         }
-        System.out.println(sb);
     }
 }
