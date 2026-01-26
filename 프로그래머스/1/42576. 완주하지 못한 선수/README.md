e370db4d17edfbaf69f1c71493625ddf5681d0f8
Eunseo Cho <100702397+eundeok9@users.noreply.github.com>
2026-01-26 20:23:08 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 73.71 ms, Memory: 66 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=javascript) 
 
 ### 성능 요약
 
-메모리: 96.7 MB, 시간: 78.54 ms
+메모리: 66 MB, 시간: 73.71 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 19일 17:20:27
+2026년 01월 26일 20:23:06
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -0,0 +1,25 @@
+function solution(participant, completion) {
+    let answer = '';
+    const myMap = new Map();
+
+    for (const part of participant){
+        if(!myMap.get(part)){
+            myMap.set(part, 1);
+        }else{
+            myMap.set(part, myMap.get(part)+1);
+        }
+    }
+
+    for(const comp of completion){
+        if(myMap.get(comp)){
+            myMap.set(comp, myMap.get(comp)-1);
+        }
+    }
+    
+    for(const part of participant){
+        if(myMap.get(part) && myMap.get(part) >=1 ){
+            answer = part;
+        }
+    }
+    return answer;
+}
